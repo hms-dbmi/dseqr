3c4abe276c296b77c771e7974d4266bd79ff72c4
alexvpickering <alexvpickering@gmail.com>
2019-04-22 12:00:01 -0700

    implement downloading ensembl transcriptome and building salmon index; closes #3
    Former-commit-id: 90d1e43a307454779f15e21708a41a6c83acfb5f

---
+++ R/build_index.R
@@ -0,0 +1,37 @@
+#' Download ensembl transcriptome and build index for salmon quantification
+#'
+#' @param species The species. Default is \code{homo_sapiens.}
+#' @param version ensembl version. Default is \code{96} (latest as of 04-22-2019).
+#'
+#' @return
+#' @export
+#'
+#' @examples
+build_index <- function(species = 'homo_sapiens', version = '96') {
+
+  indices_dir <- system.file('indices', package = 'drugseqr')
+
+  # construct ensembl url for transcriptome
+  ensembl_species <- gsub(' ', '_', tolower(species))
+  ensembl_version <- paste0('release-', version)
+  ensembl_url <- paste0('ftp://ftp.ensembl.org/pub/', ensembl_version, '/fasta/', ensembl_species, '/cdna/')
+
+  # get list of all files
+  handle <- curl::new_handle(dirlistonly=TRUE)
+  con <- curl::curl(ensembl_url, "r", handle)
+  tbl <- utils::read.table(con, stringsAsFactors=FALSE)
+  close(con)
+
+  # get transcripts cdna.all file
+  ensembl_all <- grep('cdna.all.fa.gz$', tbl[[1]], value = TRUE)
+  ensembl_url <- paste0(ensembl_url, ensembl_all)
+
+  work_dir <- getwd()
+  setwd(indices_dir)
+  curl::curl_download(ensembl_url, ensembl_all)
+
+  # build index
+  system(paste('salmon index -t', ensembl_all, '-i', ensembl_species))
+  unlink(ensembl_all)
+  setwd(work_dir)
+}
