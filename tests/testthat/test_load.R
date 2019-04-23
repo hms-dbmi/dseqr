345925c2dc9f86d3c77523e0fce35618fde0ad5a
alexvpickering <alexvpickering@gmail.com>
2019-04-23 16:28:11 -0700

    unit test load_seq helpers; closes #15
    Former-commit-id: aa38d0602116fc3d9f8ab592a530acafd62bd264

---
+++ tests/testthat/test_load.R
@@ -0,0 +1,45 @@
+context("load_seq helpers")
+
+test_that("setup_fdata expands entrez ids and makes characters", {
+
+  # list of entrezids for single transcript
+  enids <- c(5554, 11272, 100533464)
+  tx2gene <- data.frame(tx_id = 'ENST00000228811',
+                        gene_name = 'PRR4',
+                        entrezid = I(list(ENST00000228811 = enids)))
+
+  fdata <- setup_fdata(tx2gene)
+
+  expect_equal(as.character(enids), fdata$ENTREZID_HS)
+  expect_equal(class(fdata$ENTREZID_HS), 'character')
+
+})
+
+test_that("setup_fdata removes rows with duplicate gene_name and entrezid", {
+
+  # list of entrezids for two different transcripts with same gene
+  enids <- c(5554, 11272, 100533464)
+  tx2gene <- data.frame(tx_id = c('ENST00000228811', 'ENST00000228812'),
+                        gene_name = c('PRR4', 'PRR4'),
+                        entrezid = I(list(ENST00000228811 = enids, ENST00000228812 = enids)))
+
+  # just the first row
+  tx2gene_row1 <- tx2gene[1,]
+
+  fdata <- setup_fdata(tx2gene)
+  fdata_row1 <- setup_fdata(tx2gene_row1)
+
+  expect_equal(fdata, fdata_row1)
+})
+
+
+test_that("get_ensdb_package gets appropriate package name", {
+
+  ensdb_package <- get_ensdb_package('Homo sapiens', '94')
+  expect_equal(ensdb_package, 'EnsDb.Hsapiens.v94')
+
+  # handles lowercase genus name
+  ensdb_package <- get_ensdb_package('homo sapiens', '94')
+  expect_equal(ensdb_package, 'EnsDb.Hsapiens.v94')
+
+})
