d71f0ce7e144b39623bee097519a1d9a07b8421f
alexvpickering <alexvpickering@gmail.com>
2019-04-26 08:13:16 -0700

    test that 1:many PROBE:SYMBOL do not change discovered surrogate variables for #8
    Former-commit-id: da8e4e248de4048518eb35dbd4991b41bf481446

--- tests/testthat/test_diff_setup.R
+++ tests/testthat/test_diff_setup.R
@@ -31,3 +31,22 @@ test_that("model matrix is correct if test samples appear first", {
   expect_equivalent(setup$mod[, 'control'], rep(c(0, 1), each=6))
   expect_equivalent(setup$mod[, 'test'], rep(c(1, 0), each=6))
 })
+
+
+test_that("1:many PROBE:SYMBOL are removed before sva", {
+
+  # load eset and add group info
+  eset_path <- system.file('extdata', 'IBD', 'eset.rds', package = 'drugseqr')
+  eset <- readRDS(eset_path)
+  pData(eset)$group <- rep(c('test', 'control'), each=6)
+
+  # esets with uniq and duplicate rows
+  eset_uniq <- eset[1:1000, ]
+  eset_dups <- eset[rep(1:1000, each=5), ]
+
+  setup_uniq <- diff_setup(eset_uniq, svanal = TRUE, rna_seq = TRUE)
+  setup_dups <- diff_setup(eset_dups, svanal = TRUE, rna_seq = TRUE)
+
+  # discovered surrogate variables should be equal
+  expect_equal(setup_uniq$modsv, setup_dups$modsv)
+})
