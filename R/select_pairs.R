d7a13f76b1e8483388cfb18fc51713f4dd8e8cb4
alexvpickering <alexvpickering@gmail.com>
2019-04-27 15:01:06 -0700

    server working and init docs for #16
    Former-commit-id: a23ede078d8e96a37798b5dfb70b98ed10657c4f

--- R/select_pairs.R
+++ R/select_pairs.R
@@ -1,12 +1,15 @@
 
 
-#' Select contrast for an experiment.
+#' Select pairs and replicates for an experiment.
 #'
-#' Used to select a contrast (control and test group samples) for an experiment.
+#' Used to select a pairs for paired-end experiments and replicate samples. Please follow prompt, ensuring correct file name matching and
+#' end-type experiment identification.
 #'
-#' @param eset \code{ExpressionSet}
+#' @param data_dir Directory with raw and quantified RNA-Seq files.
+#' @param pdata_path Path to text file with sample annotations. Must be readable by \code{\link[data.table]{fread}}.
+#' The first column should contain sample ids that match a single raw rna-seq data file name.
 #'
-#' @return Named list of length two. In order, list items contain rows corresponding to control and test group. Names are specified group names.
+#' @return data.frame of sample annotations with integer columns \code{Pair} (if paired-end) and \code{Replicate} (if added).
 #' @export
 #'
 #' @examples
@@ -14,14 +17,23 @@
 #' data_dir <- system.file('extdata', 'IBD', package='drugseqr')
 #' pdata_path <- file.path(data_dir, 'Phenotypes.csv')
 #'
-#' select_pairs(data_dir, pdata_path)
+#' pdata <- select_pairs(data_dir, pdata_path)
 #'
 #'
 select_pairs <- function(data_dir, pdata_path) {
 
+  # TODO: validate that e.g. 2 samples selected as replicates are not also paired
+
   # setup ----
 
-  background <- '#e9305d url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAPklEQVQoU43Myw0AIAgEUbdAq7VADCQaPyww55dBKyQiHZkzBIwQLqQzCk9E4Ytc6KEPMnTBCG2YIYMVpHAC84EnVbOkv3wAAAAASUVORK5CYII=) repeat'
+  group_colors <- c("#C7E9C0", "#C6DBEF", "#FCBBA1", "#FDD0A2", "#BCBDDC", "#D9D9D9", "#F6E8C3", "#DC143C",
+                    "#A1D99B", "#9ECAE1", "#FC9272", "#FDAE6B", "#9E9AC8", "#BDBDBD", "#DFC27D", "#FFFFFF",
+                    "#C3B091", "#007FFF", "#00FFFF", "#7FFFD4", "#228B22", "#808000", "#7FFF00", "#BFFF00",
+                    "#FFD700", "#DAA520", "#FF7F50", "#FA8072","#FC0FC0", "#CC8899", "#E0B0FF", "#B57EDC", "#843179")
+
+  ncolors <- length(group_colors)
+
+  background <- 'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAPklEQVQoU43Myw0AIAgEUbdAq7VADCQaPyww55dBKyQiHZkzBIwQLqQzCk9E4Ytc6KEPMnTBCG2YIYMVpHAC84EnVbOkv3wAAAAASUVORK5CYII=) repeat'
 
   # load pdata and determine row to file correspondence
   # needs to be data.frame for ExpressionSet construction
@@ -51,10 +63,11 @@ select_pairs <- function(data_dir, pdata_path) {
             - confirm that the experiment was correctly identified as single-ended or paired-end
             - select samples to treat as a single library (if any - e.g. same sample sequenced in replicate)\n")
 
+  message("Click 'Done' to continue with salmon quantification.")
+
 
   # things we will update/return to user
-  pairs <- list()
-  dups <- list()
+  pairs <- reps <- rep(NA, nrow(pdata))
 
 
   #  user interface ----
@@ -80,8 +93,8 @@ select_pairs <- function(data_dir, pdata_path) {
       )
     ),
     miniUI::miniButtonBlock(
-      shiny::actionButton("pairs", "Pair Samples"),
-      shiny::actionButton("dups", "Mark Replicates")
+      shiny::actionButton("pair", "Pair Samples"),
+      shiny::actionButton("rep", "Mark Replicates")
     )
   )
 
@@ -90,7 +103,7 @@ select_pairs <- function(data_dir, pdata_path) {
   server <- function(input, output, session) {
 
     # make reactive state value to keep track of ctrl vs test group
-    state <- shiny::reactiveValues(ctrl = 1, pdata = 0)
+    state <- shiny::reactiveValues(pdata = 0)
 
 
 
@@ -111,8 +124,29 @@ select_pairs <- function(data_dir, pdata_path) {
       )
     })
 
-    # pdata reactive so that will update group column
+    # pdata reactive so that will update Pair/Replicate column
     pdata_r <- shiny::eventReactive(state$pdata, {
+
+      # update pdata Replicate column
+      rep_nums <- sort(unique(setdiff(reps, NA)))
+      for (rep_num in rep_nums) {
+        color <- group_colors[ncolors - rep_num]
+        rows <- which(reps == rep_num)
+        pdata[rows, 'Replicate'] <<- paste('<div style="background-color:', color, ';"></div>')
+      }
+
+      # update pdata Pair column
+      if (paired) {
+        pair_nums <- sort(unique(setdiff(pairs, NA)))
+        for (pair_num in pair_nums) {
+          color <- group_colors[pair_num]
+          rows <- which(pairs == pair_num)
+          pdata[rows, 'Pair'] <<- paste('<div style="background:', color, background, ';"></div>')
+        }
+      } else {
+        pdata[1:nrow(pdata), 'Pair'] <<- NA
+      }
+
       return(pdata)
     })
 
@@ -123,61 +157,90 @@ select_pairs <- function(data_dir, pdata_path) {
     })
 
 
-    # click 'Add' ----
+    # click 'Pair Samples' ----
 
-    shiny::observeEvent(input$add, {
+    shiny::observeEvent(input$pair, {
 
       # get rows
       rows  <- input$pdata_rows_selected
 
       # check for incomplete/wrong input
-      if (length(sels) == 2) {
-        message("Contrast is fully specified. Click 'Reset' to clear selections or 'Done' to exit.")
+      if (length(rows) < 2) {
+        message("Select at least two rows to pair.")
 
-      } else if (length(rows) == 0) {
-        message("Select rows.")
-
-      } else if (Position(function(x) setequal(x, rows), sels, nomatch = 0) > 0) {
-        message("Selection in use for control group.")
-
-      } else if (state$ctrl == 1) {
-        # add ctrl group data to sels
-        sels <<- c(sels, list(ctrl = rows))
-
-        # update pdata Group column
-        pdata[rows, 'Group'] <<- paste0('<div style="background: ', background, ';"><span>control</span></div>')
-
-        # update states to trigger updates
-        state$ctrl <- 0
-        state$pdata <- state$pdata + 1
+      } else if (!all(is.na(pairs[rows]))) {
+        message("Selected row(s) already belong to a pair. Click 'Reset' if you need to start over.")
 
       } else {
-        # add test group data to sels
-        sels <<- c(sels, list(test = rows))
+        # add rows as a pair
+        pair_num <- length(unique(setdiff(pairs, NA))) + 1
+        pairs[rows] <<- pair_num
 
-        # add group to table
-        pdata[rows, 'Group'] <<- '<div style="background-color: #e6194b;"><span>test</span></div>'
+        # update states to trigger updates
         state$pdata <- state$pdata + 1
-        state$ctrl <- 3
-        shinyjs::disable("add")
+      }
+    })
+
+    # click 'Mark Replicates' ----
+
+    shiny::observeEvent(input$rep, {
+
+      # get rows
+      rows  <- input$pdata_rows_selected
+
+      # check for incomplete/wrong input
+      if (length(rows) < 2) {
+        message("Select at least two rows to mark as replicates.")
+
+      } else {
+        # add rows as replicates
+        rep_num <- length(unique(setdiff(reps, NA))) + 1
+        reps[rows] <<- rep_num
+
+        # update states to trigger updates
+        state$pdata <- state$pdata + 1
       }
     })
 
     # click 'Done' ----
 
     shiny::observeEvent(input$done, {
-      if (state$ctrl == 0) {
-        message("Need to add test group.")
-      } else if (length(sels) == 0) {
-        message("No contrast selected.")
+      if (paired & sum(is.na(pdata$Pair)) != 0) {
+        message("All samples must be in a pair for paired-end experiments.")
       } else {
-        group <- rep(NA, ncol(eset))
-        group[sels$test] <- 'test'
-        group[sels$ctrl] <- 'control'
-        Biobase::pData(eset)$group <- group
 
-        # retain selected samples only
-        stopApp(eset[, !is.na(group)])
+        # replace html with integers identifying pairs/replicates
+        if (paired) {
+          pdata$Pair <<- pairs
+        } else {
+          pdata$Pair <<- NULL
+        }
+
+        if (all(is.na(reps))) {
+          pdata$Replicate <<- NULL
+        } else {
+          pdata$Replicate <<- reps
+        }
+
+        stopApp(pdata)
+      }
+    })
+
+    # select 'end-type' ----
+    shiny::observeEvent(input$end_type, {
+
+      # single-ended experiments are not paired, so disable
+      # hide pair column labels if select single-ended
+
+      if (grepl('^single', input$end_type)) {
+        shinyjs::disable("pair")
+        paired <<- FALSE
+        state$pdata <- state$pdata + 1
+
+      } else {
+        shinyjs::enable("pair")
+        paired <<- TRUE
+        state$pdata <- state$pdata + 1
       }
     })
 
@@ -185,13 +248,13 @@ select_pairs <- function(data_dir, pdata_path) {
     # click 'Reset' ----
 
     shiny::observeEvent(input$reset, {
-      sels <<- list()
-      pdata$Group <<- NA
+      pairs <<- c()
+      reps <<- c()
+      pdata$Pairs <<- NA
+      pdata$Replicate <<- NA
 
       # remove groups from table and reset control
-      state$ctrl <- 1
       state$pdata <- 0
-      shinyjs::enable("add")
     })
   }
 
