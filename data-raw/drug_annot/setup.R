c76c902c1e3b90337f2acd906f434be594bfcce0
alexvpickering <alexvpickering@gmail.com>
2019-05-01 17:00:26 -0700

    add Broad Repo Hub data to L1000 for #4
    Former-commit-id: 99464f5845ac69215c64e87aac073caeafd9a496
    Former-commit-id: e1ed3123d8a0e6670c5ba9c6edb3ac369f310569

--- data-raw/drug_annot/setup.R
+++ data-raw/drug_annot/setup.R
@@ -26,7 +26,7 @@ annot <- unique(annot)
 annot$pubchem_cid <- as.character(annot$pubchem_cid)
 
 
-# CMAP02 pdata
+# CMAP02 setup ----
 cmap_pdata <- readRDS('inst/extdata/CMAP02_pdata.rds')
 cmap_pdata <- destructure_title(cmap_pdata, drop=FALSE, .after=1)
 
@@ -45,7 +45,7 @@ cmap_annot <- cmap_annot %>%
   summarise_all(function(x)  {
     unqx <- unique(na.omit(x))
 
-    # keep as NA is they all are
+    # keep as NA if they all are
     if (!length(unqx)) return(NA_character_)
 
     # collapse distinct non-NA entries
@@ -59,3 +59,39 @@ all.equal(cmap_annot$title, cmap_pdata$title)
 # save as seperate annotation, removing what pdata already has
 cmap_annot <- cmap_annot %>% select(-c(title, `Pubchem CID`, pert_iname))
 saveRDS(cmap_annot, 'inst/extdata/cmap_annot.rds')
+
+
+# L1000 setup -----
+
+l1000_pdata <- readRDS('inst/extdata/L1000_pdata.rds')
+l1000_pdata <- destructure_title(l1000_pdata, drop=FALSE, .after=1)
+
+# pubchem_cid has the most matches
+sum(unique(l1000_pdata$Compound) %in% tolower(annot$pert_iname))
+sum(unique(l1000_pdata$`Pubchem CID`) %in% annot$pubchem_cid)
+
+# use cmap_pdata compound names
+l1000_annot <-  l1000_pdata %>%
+  select(title, 'Pubchem CID') %>%
+  left_join(annot, by = c('Pubchem CID' = 'pubchem_cid'))
+
+# merge rows that have the same treatment
+l1000_annot <- l1000_annot %>%
+  group_by(title) %>%
+  summarise_all(function(x)  {
+    unqx <- unique(na.omit(x))
+
+    # keep as NA if they all are
+    if (!length(unqx)) return(NA_character_)
+
+    # collapse distinct non-NA entries
+    return(paste(unqx, collapse = ' // '))
+  }) %>%
+  arrange(match(title, l1000_pdata$title))
+
+# check that annot and pdata are in same order
+all.equal(l1000_annot$title, l1000_pdata$title)
+
+# save as seperate annotation, removing what pdata already has
+l1000_annot <- l1000_annot %>% select(-c(title, `Pubchem CID`, pert_iname))
+saveRDS(l1000_annot, 'inst/extdata/l1000_annot.rds')
