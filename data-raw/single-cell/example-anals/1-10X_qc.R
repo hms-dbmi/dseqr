8a5bc2cdc747a7de83f82b77a8438cfd036ca803
alexvpickering <alexvpickering@gmail.com>
2019-05-27 15:18:47 -0700

    annotate sce with tx2gene for #55
    Former-commit-id: 005fc3577bb317fcfc79e8aab8c1256ba22846ca
    Former-commit-id: c8c686c1229e3784bc6a5e6035c28ae78be3a545
    Former-commit-id: dd723562436c68d9445d88232ed3a478f6632fc3

--- data-raw/single-cell/example-anals/1-10X_qc.R
+++ data-raw/single-cell/example-anals/1-10X_qc.R
@@ -1,27 +1,44 @@
 # adapted from: https://bioconductor.org/packages/release/workflows/vignettes/simpleSingleCell/inst/doc/tenx.html
 
 library(scater)
+library(dplyr)
+library(DropletUtils)
 
 # folder with 10X fastq files
 data_dir <- file.path('data-raw/single-cell/example-data/Run2644-10X-Lung/10X_FID12518_Normal_3hg')
 
 # this is LONG RUNNING
-drugseqr::run_alevin(data_dir)
+# drugseqr::run_alevin(data_dir)
 
 # import alevin quants
 alevin_dir <- file.path(data_dir, 'alevin_output', 'alevin')
-alevin <- tximport::tximport(file.path(alevin_dir, 'quants_mat.gz'), type = 'alevin')
+alevin <- tximport::tximport(file.path(alevin_dir, 'quants_mat.gz'), type = 'alevin')$counts
 
 # Subset to only cells in whitelist
 whitelist <- read.delim(file.path(alevin_dir, 'whitelist.txt'), header = FALSE, as.is = TRUE)$V1
-alevin$whitelist <- colnames(alevin$counts) %in% whitelist
-alevin$counts <- alevin$counts[, alevin$whitelist]
+alevin <- alevin[, colnames(alevin) %in% whitelist]
 
 # Convert to SingleCellExperiment
-sce <- SingleCellExperiment::SingleCellExperiment(assays=list(counts=alevin$counts))
+sce <- SingleCellExperiment::SingleCellExperiment(assays=list(counts=alevin))
 
-# annotate sce using EnsDB package
+# annotate sce using tx2gene (best match with cmap_es/l1000_es)
+tx2gene <- readRDS("data-raw/tx2gene/tx2gene.rds")
+tx2gene <- tx2gene %>%
+  as_tibble() %>%
+  dplyr::select(-c(tx_id, entrezid)) %>%
+  group_by(gene_id) %>%
+  summarise_all(unique) %>%
+  ungroup()
 
+rowData(sce) <- tibble(gene_id = gsub('\\.[0-9]+$', '', row.names(sce))) %>%
+  left_join(tx2gene, by = 'gene_id')
 
+# appends _ID to any non-unique values of names
+rownames(sce) <- scater::uniquifyFeatureNames(ID = rowData(sce)$gene_id, names = rowData(sce)$gene_name)
+
+# mitochondrial gene
+summary(rowData(sce)$seq_name=="MT")
+# Mode   FALSE    TRUE
+# logical   20271      13
 
 
