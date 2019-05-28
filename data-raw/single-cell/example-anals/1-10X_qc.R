fb00491d2c3c8f23a9b2f0ca833c921b62e8f928
alexvpickering <alexvpickering@gmail.com>
2019-05-28 10:04:27 -0700

    add plots and normalize libraries for #55
    Former-commit-id: 1f410e454059a613c4d0f652353fbfc054fd975b
    Former-commit-id: 059b6c8eeb21024dc7ff93dcfd78aa66fe8cbb1c
    Former-commit-id: bcdc3377deb77435114402f836efa9c510cf8db0

--- data-raw/single-cell/example-anals/1-10X_qc.R
+++ data-raw/single-cell/example-anals/1-10X_qc.R
@@ -4,8 +4,10 @@ library(scater)
 library(dplyr)
 library(DropletUtils)
 
+# load/annotate data from alevin ----
+
 # folder with 10X fastq files
-data_dir <- file.path('data-raw/single-cell/example-data/Run2644-10X-Lung/10X_FID12518_Normal_3hg')
+data_dir <- file.path('data-raw/single-cell/example-data/Run2644-10X-Lung')
 
 # this is LONG RUNNING
 # drugseqr::run_alevin(data_dir)
@@ -14,9 +16,8 @@ data_dir <- file.path('data-raw/single-cell/example-data/Run2644-10X-Lung/10X_FI
 alevin_dir <- file.path(data_dir, 'alevin_output', 'alevin')
 alevin <- tximport::tximport(file.path(alevin_dir, 'quants_mat.gz'), type = 'alevin')$counts
 
-# Subset to only cells in whitelist
+# final alevin whitelist
 whitelist <- read.delim(file.path(alevin_dir, 'whitelist.txt'), header = FALSE, as.is = TRUE)$V1
-alevin <- alevin[, colnames(alevin) %in% whitelist]
 
 # Convert to SingleCellExperiment
 sce <- SingleCellExperiment::SingleCellExperiment(assays=list(counts=alevin))
@@ -27,8 +28,7 @@ tx2gene <- tx2gene %>%
   as_tibble() %>%
   dplyr::select(-c(tx_id, entrezid)) %>%
   group_by(gene_id) %>%
-  summarise_all(unique) %>%
-  ungroup()
+  summarise_all(unique)
 
 rowData(sce) <- tibble(gene_id = gsub('\\.[0-9]+$', '', row.names(sce))) %>%
   left_join(tx2gene, by = 'gene_id')
@@ -36,9 +36,41 @@ rowData(sce) <- tibble(gene_id = gsub('\\.[0-9]+$', '', row.names(sce))) %>%
 # appends _ID to any non-unique values of names
 rownames(sce) <- scater::uniquifyFeatureNames(ID = rowData(sce)$gene_id, names = rowData(sce)$gene_name)
 
-# mitochondrial gene
-summary(rowData(sce)$seq_name=="MT")
-# Mode   FALSE    TRUE
-# logical   20271      13
+# alevin does initial knee-based whitelisting then refines based on mtRNA, rRNA, etc
+# see what things look like before/after final whitelisting
+sce <- calculateQCMetrics(sce, feature_controls=list(Mito=which(rowData(sce)$seq_name=="MT")))
+par(mfrow=c(1,4))
+hist(sce$log10_total_counts, breaks=20, col="grey80",
+     xlab="Log-total UMI count")
+hist(sce$log10_total_features_by_counts, breaks=20, col="grey80",
+     xlab="Log-total number of expressed features")
+hist(sce$pct_counts_Mito, breaks=20, col="grey80",
+     xlab="Percent mito reads")
+hist(sce$pct_counts_Mito[colnames(sce) %in% whitelist], breaks=20, col="grey80",
+     xlab="Percent mito reads (whitelist)")
+
+# subset by alevin whitelist
+sce <- sce[, colnames(sce) %in% whitelist]
+
+# look at most highly expressed genes
+# expect dominated by mitochondrial/ribosomal
+ave <- calcAverage(sce)
+rowData(sce)$AveCount <- ave
+hist(log10(ave), col="grey80", breaks = 100)
+plotHighestExprs(sce)
+
+# normalizing for cells-specific biases ----
+library(scran)
+library(BiocSingular)
+set.seed(1000)
+clusters <- quickCluster(sce, use.ranks=FALSE, BSPARAM=IrlbaParam())
+table(clusters)
+
+# sizefactors should correlated well with total counts
+# indicates capture efficiency/sequencing depth are the major biases
+plot(sce$total_counts, sizeFactors(sce), log="xy")
+
+# normalize based on size-factors
+sce <- normalize(sce)
 
 
