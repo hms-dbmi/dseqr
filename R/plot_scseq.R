8eef62c95a4214045b85741240ee5f71f7a49fb2
alexvpickering <alexvpickering@gmail.com>
2019-05-29 14:49:43 -0700

    init tsne_scseq_whitelist to inspect whitelist cells/metrics for #60
    Former-commit-id: 58eb1640eeaa46c7d089dabb01e1ffb171c4f41a
    Former-commit-id: 0727fd7a0c3d4105855d86b257a5a4641ec9b877
    Former-commit-id: cba1316b43a30ceb7e7c8dae995959ebf5bdb719

--- R/plot_scseq.R
+++ R/plot_scseq.R
@@ -30,7 +30,7 @@ geom_hist_whitelist <- function(sce_df, x, xlab = '', ylab = '') {
 #' @export
 #'
 #' @examples
-hist_scseq <- function(sce) {
+hist_scseq_whitelist <- function(sce) {
 
   # make sure have qc metrics
   sce <- qc_scseq(sce)
@@ -50,3 +50,95 @@ hist_scseq <- function(sce) {
   suppressMessages(legend <- cowplot::get_legend(nexp + ggplot2::theme(legend.position="top")))
   suppressMessages(cowplot::plot_grid(legend, NULL, ncnt, nexp, ribo, mito, nrow = 3, rel_heights = c(0.2, 1, 1)))
 }
+
+
+#' tSNE plots of whitelisted cells
+#'
+#' @param sce
+#'
+#' @return
+#' @export
+#'
+#' @examples
+tsne_scseq_whitelist <- function(sce) {
+
+  # make sure have qc metrics
+  sce <- qc_scseq(sce)
+
+  # uses denoised expression matrix
+  set.seed(1000)
+  sce <- scater::runTSNE(sce, use_dimred="PCA")
+
+  suppressMessages(tsne_white <- scater::plotTSNE(sce, colour_by='whitelist', point_alpha = 1) +
+                     ggplot2::scale_fill_manual(values = c("#E31A1C", "#FFFFCC")) +
+                     guides(fill=guide_legend(title='whitelist')) +
+                     theme(legend.position = 'top',
+                           axis.text=element_blank(),
+                           axis.ticks=element_blank()) +
+                     xlab('') +
+                     ylab(''))
+
+  # make really low mito/ribo content more prominent as well
+  mito_scale <- get_diverge(sce$pct_counts_mito, 'Mitochondrial percent')
+  ribo_scale <- get_diverge(sce$pct_counts_ribo, 'Ribosomal percent')
+
+  suppressMessages(tsne_mito <- scater::plotTSNE(sce, colour_by='pct_counts_mito', point_alpha = 1) +
+                     mito_scale +
+                     theme(legend.position = 'top',
+                           axis.text=element_blank(),
+                           axis.ticks=element_blank()) +
+                     xlab(''))
+
+  suppressMessages(tsne_ribo <- scater::plotTSNE(sce, colour_by='pct_counts_ribo', point_alpha = 1) +
+                     ribo_scale +
+                     theme(legend.position = 'top',
+                           axis.title.x=element_blank(),
+                           axis.text=element_blank(),
+                           axis.ticks=element_blank()) +
+                     xlab('') +
+                     ylab(''))
+
+  suppressMessages(tsne_ncounts <- scater::plotTSNE(sce, colour_by='log10_total_counts', point_alpha = 1) +
+                     ggplot2::scale_fill_distiller(palette = 'YlOrRd', name = 'Log of total counts') +
+                     theme(legend.position = 'top',
+                           axis.text=element_blank(),
+                           axis.ticks=element_blank()) +
+                     ylab(''))
+
+
+  suppressMessages(tsne_nexp <- scater::plotTSNE(sce, colour_by='log10_total_features_by_counts', point_alpha = 1) +
+                     ggplot2::scale_fill_distiller(palette = 'YlOrRd', name = 'Log of expressed genes') +
+                     theme(legend.position = 'top',
+                           axis.text=element_blank(),
+                           axis.ticks=element_blank()) +
+                     xlab('') +
+                     ylab(''))
+
+  suppressWarnings(cowplot::plot_grid(tsne_white, NULL, tsne_mito, tsne_ribo, tsne_ncounts, tsne_nexp, nrow = 3))
+}
+
+
+
+#' Get a continuous color scale that emphasizes 0-10th and 90-100th percentiles.
+#'
+#' @param x Numeric vector
+#' @param name Character, name of scale
+#'
+#' @return ggplot2 \code{ScaleContinuous}
+#' @export
+#'
+#' @examples
+get_diverge <- function(x, name) {
+  range01 <- function(x){(x-min(x))/(max(x)-min(x))}
+  qx <- quantile(x, probs = seq(0, 1, 0.1))
+
+  diverge <- ggplot2::scale_fill_gradientn(
+    name = name,
+    colors = rev(brewer.pal(9, "Spectral")),
+    values = range01(c(seq(qx[1], qx[2], length.out = 25),
+                       seq(qx[2], qx[10], length.out = 25),
+                       seq(qx[10], qx[11], length.out = 25))))
+
+  return(diverge)
+}
+
