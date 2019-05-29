98e989760a78c9e026bd515fc606a6126ce54f05
alexvpickering <alexvpickering@gmail.com>
2019-05-29 16:18:57 -0700

    updates to example sc anal for #58, #59, #60
    Former-commit-id: f7f860d64e63f460fd21e5f4faeed65b0da6152e
    Former-commit-id: e1b3d331c8449ce88d98ce06270601059f8cb307
    Former-commit-id: a3b5a5549baef1698cbeb06619e127e5a2227308

--- data-raw/single-cell/example-anals/1-sc_qc.R
+++ data-raw/single-cell/example-anals/1-sc_qc.R
@@ -17,66 +17,35 @@ data_dir <- 'data-raw/single-cell/example-data/Run2644-10X-Lung/10X_FID12518_Nor
 # drugseqr::run_alevin(data_dir)
 
 # import alevin quants
-sce <- drugseqr::load_scseq(data_dir)
+sce <- load_scseq(data_dir)
+
+# run qc
+sce <- qc_scseq(sce)
 
 # alevin does initial knee-based whitelisting then refines based on mtRNA, rRNA, etc
 # see what things look like before/after final whitelisting
-sce <- scater::calculateQCMetrics(sce, feature_controls=list(mito = which(row.names(sce) %in% sce@metadata$mrna),
-                                                             ribo = which(row.names(sce) %in% sce@metadata$rrna)))
+hist_scseq_whitelist(sce)
 
-sce_df <- tibble(log10_total_counts = sce$log10_total_counts,
-                 log10_total_features_by_counts = sce$log10_total_features_by_counts,
-                 pct_counts_ribo = sce$pct_counts_ribo,
-                 pct_counts_mito = sce$pct_counts_mito,
-                 whitelist = sce$whitelist)
-
-ribo <- ggplot(sce_df, aes(x=pct_counts_ribo, fill=whitelist)) +
-  theme_minimal() +
-  scale_fill_manual(values = c("#E41A1C", "#377EB8")) +
-  geom_histogram(position="identity", colour="black", alpha = 0.5, size = 0.05) +
-  xlab('Proportion of reads in ribosomal genes') +
-  theme(legend.position = "none")
-
-mito <- ggplot(sce_df, aes(x=pct_counts_mito, fill=whitelist)) +
-  theme_minimal() +
-  scale_fill_manual(values = c("#E41A1C", "#377EB8")) +
-  geom_histogram(position="identity", colour="black", alpha = 0.5, size = 0.05) +
-  xlab('Proportion of reads in mitochondrial genes') +
-  theme(legend.position = "none") +
-  ylab('')
-
-ncounts <- ggplot(sce_df, aes(x=log10_total_counts, fill=whitelist)) +
-  theme_minimal() +
-  scale_fill_manual(values = c("#E41A1C", "#377EB8")) +
-  geom_histogram(position="identity", colour="black", alpha = 0.5, size = 0.05) +
-  xlab('Log-total UMI count') +
-  theme(legend.position = "none")
-
-nexp <- ggplot(sce_df, aes(x=log10_total_features_by_counts, fill=whitelist)) +
-  theme_minimal() +
-  scale_fill_manual(values = c("#E41A1C", "#377EB8")) +
-  geom_histogram(position="identity", colour="black", alpha = 0.5, size = 0.05) +
-  xlab('Log-total number of expressed features') +
-  theme(legend.position = "none") +
-  ylab('')
-
-legend <- get_legend(nexp + theme(legend.position="top"))
-plot_grid(legend, NULL, ncounts, nexp, ribo, mito, nrow = 3, rel_heights = c(0.2, 1, 1))
-
-# subset by alevin whitelist
+# first normalize and stabilize
 sce.full <- sce
-sce <- sce[, sce$whitelist]
+sce.full <- norm_scseq(sce.full)
+sce.full <- stabilize_scseq(sce.full)
 
-# look at most highly expressed genes
+# show tSNE based on whitelist and whitelist metrics
+tsne_scseq_whitelist(sce.full)
+
+# subset by alevin whitelist and re-norm/stabilize
+sce <- sce[, sce$whitelist]
+sce <- norm_scseq(sce)
+sce <- stabilize_scseq(sce)
+
+# look at most highly expressed genes ----
+
 # expect dominated by mitochondrial/ribosomal
 ave <- scater::calcAverage(sce)
 rowData(sce)$AveCount <- ave
 hist(log10(ave), col="grey80", breaks = 100)
 scater::plotHighestExprs(sce)
-
-# normalizing for cells-specific biases ----
-
-sce <- norm_scseq(sce)
 
 # sizefactors should correlated well with total counts
 # indicates capture efficiency/sequencing depth are the major biases
@@ -105,35 +74,25 @@ head(top.dec)
 plotExpression(sce, features=rownames(top.dec)[1:15])
 
 # dimensionality reduction -----
-set.seed(1000)
-sce <- denoisePCA(sce, technical=new.trend, BSPARAM=IrlbaParam())
-ncol(reducedDim(sce, "PCA"))
-
-plot(attr(reducedDim(sce), "percentVar"), xlab="PC",
-     ylab="Proportion of variance explained")
-abline(v=ncol(reducedDim(sce, "PCA")), lty=2, col="red")
-
-plotPCA(sce, ncomponents=3, colour_by="log10_total_features_by_counts")
 
 # tSNE version
 set.seed(1000)
-sce <- runTSNE(sce, use_dimred="PCA", perplexity=30)
-plotTSNE(sce, colour_by="log10_total_features_by_counts")
+sce <- runTSNE(sce, use_dimred="PCA")
 
 # clustering with graphs ----
 snn.gr <- buildSNNGraph(sce, use.dimred="PCA")
 clusters <- igraph::cluster_walktrap(snn.gr)
-sce$Cluster <- factor(clusters$membership)
-table(sce$Cluster)
+sce$cluster <- factor(clusters$membership)
+table(sce$cluster)
 
-cluster.mod <- clusterModularity(snn.gr, sce$Cluster, get.values=TRUE)
-log.ratio <- log2(cluster.mod$observed/cluster.mod$expected + 1)
+# using original tSNE plot show clusters of whitelisted cells
+sce.full$cluster <- NA
+sce.full$cluster[sce.full$whitelist] <- sce$cluster
+sce.full$cluster <- as.factor(sce.full$cluster)
+plotTSNE(sce.full[, sce.full$whitelist], colour_by="cluster")
 
-library(pheatmap)
-pheatmap(log.ratio, cluster_rows=FALSE, cluster_cols=FALSE,
-         color=colorRampPalette(c("white", "blue"))(100))
-
-plotTSNE(sce, colour_by="Cluster")
+# this re-does the tSNE so can't compare to original anymore
+plotTSNE(sce, colour_by="cluster")
 
 # Marker gene detection of clusters -----
 # only upregulated as more useful for positive id of cell type
@@ -153,7 +112,7 @@ plotHeatmap(sce, features=chosen, exprs_values="logcounts",
 # doesn't automate well, just a visual check
 # personal note: may be useful to not conclude that one sample has cluster and other doesn't
 
-pass_qc <- colnames(sce.full) %in% whitelist
+pass_qc <- colnames(sce.full) %in% sce.full$whitelist
 
 lost <- calcAverage(counts(sce.full)[, !pass_qc])
 kept <- calcAverage(counts(sce.full)[, pass_qc])
@@ -171,7 +130,7 @@ plot(capped.lost, capped.kept, xlab="Average count (discarded)",
 # doesn't automate well, good to check that not relying on possibly double population for downstream analysis
 
 # relies on good clusters
-dbl.out <- doubletCluster(sce, sce$Cluster)
+dbl.out <- doubletCluster(sce, sce$cluster)
 dbl.out
 
 dbl.markers <- markers[["1"]]
