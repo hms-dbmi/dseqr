04cbc87dd1fb26dc5d7445f8a22b317ff1b78c0c
alexvpickering <alexvpickering@gmail.com>
2019-06-03 15:06:49 -0700

    update seurat example anal
    Former-commit-id: 4f66c1e60a93837f3d2bc2b919a2b5d397bb210a
    Former-commit-id: 8c858cb4bc18a81a2048ed082aef9d65489ec1b6
    Former-commit-id: fad94b5e77d3ad18b9a9514b793f497f50c2ea88

--- data-raw/single-cell/example-anals/5-seurat.R
+++ data-raw/single-cell/example-anals/5-seurat.R
@@ -1,36 +1,23 @@
-library(Seurat)
 library(drugseqr)
-library(dplyr)
 
 data_dir <- 'data-raw/single-cell/example-data/Run2644-10X-Lung/10X_FID12518_Normal_3hg'
-srt <- load_scseq(data_dir)
+scseq.full <- load_scseq(data_dir)
 
-# subset by alevin whitelist
-srt <- srt[, srt$whitelist]
-
-
-# store mitochondrial percentage
-qcgenes <- load_scseq_qcgenes()
-srt <- PercentageFeatureSet(srt, pattern = paste0('^', scqc_genes$mrna, '$', collapse = '|'), col.name = "percent.mt")
+# qc plots
+hist_scseq_whitelist(scseq.full)
 
 # run sctransform
-srt <- SCTransform(srt, vars.to.regress = "percent.mt", verbose = FALSE)
-srt <- SCTransform(srt, verbose = FALSE)
+scseq.full <- preprocess_scseq(scseq.full)
+tsne_scseq_whitelist(scseq.full)
 
+# subset by alevin whitelist
+scseq <- scseq.full[, scseq.full$whitelist]
+scseq <- preprocess_scseq(scseq)
 
-# These are now standard steps in the Seurat workflow for visualization and clustering
-srt <- RunPCA(srt, verbose = FALSE)
-srt <- RunUMAP(srt, dims = 1:30, verbose = FALSE)
+# add clusters
+scseq <- add_scseq_clusters(scseq)
 
-srt <- FindNeighbors(srt, dims = 1:30, verbose = FALSE)
-srt <- FindClusters(srt, verbose = FALSE)
-DimPlot(srt, label = TRUE) + NoLegend()
+# run tnse and explore clusters
+scseq <- run_tsne(scseq)
+explore_scseq_clusters(scseq)
 
-
-
-markers <- FindAllMarkers(srt, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-top_markers <- markers %>% group_by(cluster) %>% top_n(n = 2, wt = avg_logFC)
-
-VlnPlot(srt, features = c("C1QC", "APOC1"))
-
-FeaturePlot(srt, features = top_markers$gene)
