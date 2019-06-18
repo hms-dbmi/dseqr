64d6c4156994002d907175588d33911717f1618c
alexvpickering <alexvpickering@gmail.com>
2019-06-18 15:35:59 -0700

    init example workflow using kallisto/bustools/SoupX for #76
    Observations:
    
    - kallisto is much faster than alevin
    - sense that downstream results are worse with kallisto (e.g. clusters have fewer marker genes that clearly segregate and identify a cluster as a particular cell type)
    - kallisto lacks automated cell filtering
    - alevin has seemingly quite good automated cell filtering
    - there isn't much else on offer for automated cell filtering (cellity - more hands on)
    
    My current inclination is to keep using alevin and work with the devs to make it more ammenable to use with SoupX.
    
    
    Former-commit-id: dc6f47726a46f01aeaeb5e3b9c0b11afbc4d0264
    Former-commit-id: 4f51f52b3d8eb6833f653b6e2d4e80b7a9ae0b1d
    Former-commit-id: c362a624a2fc35828d84bbd2ceaa7f8c7aba922e

---
+++ data-raw/single-cell/example-anals/6-soup.R
@@ -0,0 +1,62 @@
+library(drugseqr)
+
+ctrl_dir <- 'data-raw/single-cell/example-data/Run2644-10X-Lung/10X_FID12518_Normal_3hg'
+test_dir <- 'data-raw/single-cell/example-data/Run2643-10X-Lung/10X_FID12518_Diseased_3hg'
+index_path <- 'data-raw/single-cell/kallisto-args/Homo_sapiens.GRCh38.cdna.all.release-94_k31.idx'
+whitelist_path <- 'data-raw/single-cell/kallisto-args/10xv2_whitelist.txt'
+
+# load raw counts and remove non expressed genes
+ctrl_counts <- read_kallisto(ctrl_dir)
+test_counts <- read_kallisto(test_dir)
+
+ctrl_counts <- ctrl_counts[Matrix::rowSums(ctrl_counts) > 0, ]
+test_counts <- test_counts[Matrix::rowSums(test_counts) > 0, ]
+
+# calculate empty droplets
+ctrl_empty <- get_empty(ctrl_counts)
+test_empty <- get_empty(test_counts)
+
+
+# strain soup
+ctrl_scseq <- strain_scseq(ctrl_counts, ctrl_empty, 'ctrl')
+test_scseq <- strain_scseq(test_counts, test_empty, 'test')
+
+# quality control cells
+ctrl_scseq <- qc_scseq(ctrl_scseq)
+test_scseq <- qc_scseq(test_scseq)
+
+
+# sctransform norm/variance stabilization
+ctrl_scseq <- preprocess_scseq(ctrl_scseq)
+test_scseq <- preprocess_scseq(test_scseq)
+
+# add clusters
+ctrl_scseq <- add_scseq_clusters(ctrl_scseq, resolution = 1.6)
+test_scseq <- add_scseq_clusters(test_scseq)
+
+# run tnse
+ctrl_scseq <- run_tsne(ctrl_scseq)
+test_scseq <- run_tsne(test_scseq)
+
+# get markers and explore
+ctrl_markers <- get_scseq_markers(ctrl_scseq)
+test_markers <- get_scseq_markers(test_scseq)
+
+explore_scseq_clusters(ctrl_scseq, ctrl_markers)
+explore_scseq_clusters(test_scseq, test_markers)
+
+# integrated analysis ------
+
+# perform integration
+anchors <- FindIntegrationAnchors(object.list = list(ctrl_scseq, test_scseq))
+combined <- IntegrateData(anchors)
+DefaultAssay(combined) <- "integrated"
+combined <- ScaleData(combined, verbose = FALSE)
+
+# get clusters/tsne/markers
+combined <- add_scseq_clusters(combined)
+combined <- run_tsne(combined)
+markers <- get_scseq_markers(combined)
+
+# look at how integration changes cell groupings
+explore_scseq_clusters(combined, markers)
