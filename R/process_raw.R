2b1f23b1de6565e112327405e21d08b7713d7648
alexvpickering <alexvpickering@gmail.com>
2019-06-21 08:48:26 -0700

    migrate indices to kallisto for #79
    Former-commit-id: 4a9a71a4088fa4b2a421003866e62f9c4a3fcb27
    Former-commit-id: 59239d80b4603d94c6c68a6e3c87ad7dbbad1984
    Former-commit-id: ce5ee8fbf3841da9525f622e48e7c9ed2d24c2f9

--- R/process_raw.R
+++ R/process_raw.R
@@ -1,27 +1,22 @@
-#' Download ensembl transcriptome and build index for salmon quantification
+#' Download ensembl transcriptome and build index for kalliso quantification
 #'
-#' This index is used for bulk RNA seq quantification. See \code{\link{build_gencode_index}} for single cell RNA-seq equivalent.
+#' This index is used for RNA seq quantification.
 #'
-#' @param species The species. Default is \code{homo_sapiens.}
+#' @param species The species. Default is \code{homo_sapiens}.
 #' @param release ensembl release. Default is \code{94} (latest in release for AnnotationHub -
-#'   needs to match with \code{\link{build_ensdb}}) and corresponds to Gencode release 29 for \code{\link{build_gencode_index}}.
-#' @param indices_dir Directory with salmon indices.
-#' @param command System command for salmon.
+#'   needs to match with \code{\link{build_ensdb}}).
+#' @param indices_dir Directory to put kallisto indices.
 #'
 #' @return NULL
 #' @export
 #'
 #' @examples
-#' # build salmon index for humans
-#' build_ensdb_index()
+#' # build kallist index for humans
+#' indices_dir <- 'data-raw/indices/kallisto'
+#' build_ensdb_index(indices_dir)
 #'
-build_ensdb_index <- function(indices_dir, species = 'homo_sapiens', release = '94', command = 'salmon') {
+build_kallisto_index <- function(indices_dir, species = 'homo_sapiens', release = '94') {
 
-  salmon_version <- get_salmon_version(command)
-  if (salmon_version == '0.14.0')
-    stop('EnsemblDb indices not yet implemented for salmon 0.14.0')
-
-  indices_dir <- file.path(indices_dir, salmon_version, 'ensdb')
   if (!dir.exists(indices_dir))
     dir.create(indices_dir, recursive = TRUE)
 
@@ -37,103 +32,21 @@ build_ensdb_index <- function(indices_dir, species = 'homo_sapiens', release = '
   close(con)
 
   # get transcripts cdna.all file
-  ensembl_all <- grep('cdna.all.fa.gz$', tbl[[1]], value = TRUE)
-  ensembl_url <- paste0(ensembl_url, ensembl_all)
+  ensembl_fasta <- grep('cdna.all.fa.gz$', tbl[[1]], value = TRUE)
+  ensembl_url <- paste0(ensembl_url, ensembl_fasta)
 
   work_dir <- getwd()
   setwd(indices_dir)
-  curl::curl_download(ensembl_url, ensembl_all)
+  curl::curl_download(ensembl_url, ensembl_fasta)
 
   # build index
-  tryCatch(system2(command, args=c('index',
-                                   '-t', ensembl_all,
-                                   '-i', ensembl_species)),
-           error = function(err) {err$message <- 'Is salmon installed and on the PATH?'; stop(err)})
+  index_fname <- gsub('fa.gz$', paste0(ensembl_release, '_k31.idx'), ensembl_fasta)
+  tryCatch(system2('kallisto', args=c('index',
+                                   '-i', index_fname,
+                                   ensembl_fasta)),
+           error = function(err) {err$message <- 'Is kallisto installed and on the PATH?'; stop(err)})
 
-  unlink(ensembl_all)
-  setwd(work_dir)
-}
-
-#' Download ensembl transcriptome and build index for salmon quantification
-#'
-#' This index is used for single cell RNA-seq quantification. See \code{\link{build_ensdb_index}} for bulk RNA-seq equivalent.
-#'
-#' @param indices_dir directory to place indices in.
-#' @param species The species. Default is \code{homo_sapiens.}
-#' @param release gencode release. Default is \code{29} (matches ensembl release 94 for \code{\link{build_ensdb_index}})).
-#'
-#' @return NULL
-#' @export
-#'
-#' @examples
-#' # build salmon alevin index for humans
-#' indices_dir <- 'data-raw/indices'
-#' build_gencode_index(indices_dir)
-#'
-build_gencode_index <- function(indices_dir, species = 'human', release = '29', command = 'salmon') {
-
-  salmon_version <- get_salmon_version(command)
-  salmon_old <- salmon_lt_0.14.0(salmon_version)
-
-  # newer salmon uses decoys in index
-  if (salmon_old)
-    build_gencode_index_no_decoys(indices_dir, species, release)
-  else
-    build_gencode_index_decoys(indices_dir, species, release)
-}
-
-build_gencode_index_decoys <- function(indices_dir, species, release) {
-
-  indices_dir <- file.path(indices_dir, '0.14.0', 'gencode')
-  if (!dir.exists(indices_dir)) dir.create(indices_dir, recursive = TRUE)
-
-  if (species != 'human' | release != '29')
-    stop('Only implemented for human gencode v29.')
-
-  gencode_file <- 'human_GENCODEv29.tar.gz'
-  gencode_url <- paste0('https://s3.us-east-2.amazonaws.com/drugseqr/', gencode_file)
-
-  work_dir <- getwd()
-  setwd(indices_dir)
-
-  download.file(gencode_url, gencode_file)
-  utils::untar(gencode_file)
-
-  # build index
-  tryCatch(system2('salmon', args=c('index',
-                                    '-t', 'human_GENCODEv29/gentrome.fa',
-                                    '--gencode',
-                                    '-d', 'human_GENCODEv29/decoys.txt',
-                                    '-i', species)),
-           error = function(err) {err$message <- 'Is salmon installed and on the PATH?'; stop(err)})
-
-
-  unlink(c(gencode_file, 'human_GENCODEv29'), recursive = TRUE)
-  setwd(work_dir)
-}
-
-
-build_gencode_index_no_decoys <- function(indices_dir, species, release) {
-
-  indices_dir <- file.path(indices_dir, '0.13.1', 'gencode')
-  if (!dir.exists(indices_dir)) dir.create(indices_dir, recursive = TRUE)
-
-  # construct ensembl url for protein coding transcriptome
-  gencode_file <- paste0('gencode.v', release, '.pc_transcripts.fa.gz')
-  gencode_url <- paste0('ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_', species, '/release_', release, '/', gencode_file)
-
-  work_dir <- getwd()
-  setwd(indices_dir)
-  curl::curl_download(gencode_url, gencode_file)
-
-  # build index
-  tryCatch(system2('salmon', args=c('index',
-                                    '-t', gencode_file,
-                                    '--gencode',
-                                    '-i', species)),
-           error = function(err) {err$message <- 'Is salmon installed and on the PATH?'; stop(err)})
-
-  unlink(gencode_file)
+  unlink(ensembl_fasta)
   setwd(work_dir)
 }
 
