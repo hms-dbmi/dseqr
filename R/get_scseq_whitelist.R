a001bf28623d6d86f4d5a0319b0a6285568a6508
alexvpickering <alexvpickering@gmail.com>
2019-06-21 07:11:38 -0700

    automate whitelist for #79
    Uses similar whitelist approach as salmon alevin:
    
    1) pick knee point in barcode rank plot
    
    2) take at least 1000 barcodes below knee and label as low quality
    
    3) divide barcodes above knee in half based on total counts
       top half is high quality, bottom half is ambiguous.
    
    4) also set outliers above the knee as low quality based on:
        a) PCA of QC metrics
        b) high mitochondrial content
    
    4) train naive bayes model to predict quality from QC metrics
    
    5) classify ambiguous cases
    
    
    Former-commit-id: a85d817c25f2af0d9b3f76e48f2e93cc624b5514
    Former-commit-id: fdadbba216e95a82c258a72b21db988ca126a29f
    Former-commit-id: 9301ad4bb6e94a0f233b6166e7208c47f12d0775

---
+++ R/get_scseq_whitelist.R
@@ -0,0 +1,128 @@
+#' Get whitelist for high quality cells
+#'
+#' @param counts dgTMatrix of counts for all barcodes.
+#' @param data_dir Directory that contains kallisto \code{'bus_output'} folder that counts were loaded from.
+#'   whitelist is loaded from here if previously saved.
+#'
+#' @return Character vector of barcodes called as high quality cells.
+#' @export
+#' @keywords internal
+#'
+#' @examples
+get_scseq_whitelist <- function(counts, data_dir) {
+
+  # check for previous whitelist
+  whitelist_path <- file.path(data_dir, 'bus_output', 'whitelist.txt')
+  if (file.exists(whitelist_path)) {
+    whitelist <- readLines(whitelist_path)
+    return(whitelist)
+  }
+
+  # based on salmon alevin paper
+  # get knee and keep at least 1000 below it
+  knee <- get_knee(counts)
+  ncount <- Matrix::colSums(counts)
+  ncount.ord <- order(ncount, decreasing = TRUE)
+
+  counts <- counts[, ncount.ord]
+  ncount <- ncount[ncount.ord]
+  nabove <- sum(ncount > knee)
+  nbelow <- max(0.2*nabove, 1000)
+  keep <- 1:(nabove + nbelow)
+
+  # add qc metrics
+  scseq <- Seurat::CreateSeuratObject(counts[, keep])
+  sce <- srt_to_sce(scseq)
+  sce <- add_scseq_qc_metrics(sce)
+
+  # detect outliers based on PCA and mitochondrial content above knee
+  sce_above <- scater::runPCA(sce[, 1:nabove], use_coldata = TRUE, detect_outliers = TRUE)
+  mito.drop <- scater::isOutlier(sce_above$pct_counts_mito, nmads=3, type="higher")
+  outl.drop <- sce_above$outlier
+
+  # below knee is low quality
+  df <- as.data.frame(sce@colData)
+  df$quality <- NA
+  df$quality[df$total_counts <= knee] <- 'low'
+
+  # devide above evenly into high and ambiguous
+  midpnt <- round(nabove/2)
+  df$quality[1:midpnt] <- 'high'
+  df$quality[(midpnt+1):nabove] <- 'ambig'
+
+  # set outliers above the knee as low quality
+  df$quality[colnames(sce_above)[mito.drop]] <- 'low'
+  df$quality[colnames(sce_above)[outl.drop]] <- 'low'
+
+  # run model/get preds
+  df$quality <- factor(df$quality)
+  train <- df[df$quality != 'ambig', ]
+  test  <- df[df$quality == 'ambig', ]
+
+  nb_model <- e1071::naiveBayes(quality ~ ., data = train)
+  np_preds <- predict(nb_model, newdata = test)
+
+  # determine what to keep and save for future
+  test$quality <- np_preds
+  df <- rbind(train, test)
+  whitelist <- row.names(df)[df$quality == 'high']
+
+  writeLines(whitelist, whitelist_path)
+  return(whitelist)
+}
+
+
+#' Get Roryk knee point and plot on barcode rank plot
+#'
+#' @param counts dgTMatrix of counts for all barcodes.
+#'
+#' @return count value corresponding to Roryk knee point
+#' @export
+#' @keywords internal
+#'
+#' @examples
+get_knee <- function(counts) {
+  bcrank <- DropletUtils::barcodeRanks(counts)
+
+  # Only showing unique points for plotting speed.
+  uniq <- !duplicated(bcrank$rank)
+
+  plot(bcrank$rank[uniq], bcrank$total[uniq], log="xy",
+       xlab="Rank", ylab="Total UMI count", cex.lab=1.2)
+
+  roryk_knee <- pick_roryk_cutoff(bcrank$total)
+
+  abline(h=metadata(bcrank)$inflection, col="darkgreen", lty=2)
+  abline(h=metadata(bcrank)$knee, col="dodgerblue", lty=2)
+  abline(h=roryk_knee, col="red", lty=2)
+
+  legend("bottomleft", legend=c("Inflection", "Knee", "Roryk Knee"),
+         col=c("darkgreen", "dodgerblue", "red"), lty=2, cex=1.2)
+
+  return(metadata(bcrank)$inflection)
+}
+
+#' Pick Roryk knee point
+#'
+#' from https://github.com/COMBINE-lab/salmon/issues/362
+#'
+#' @param bcs Vector of total counts for all barcodes
+#'
+#' @return count value corresponding to Roryk knee point
+#' @export
+#' @keywords internal
+#'
+#' @examples
+pick_roryk_cutoff <- function(bcs){
+  bcs_hist = hist(log10(bcs), plot=FALSE, n=50)
+  mids = bcs_hist$mids
+  vals = bcs_hist$count
+  wdensity = vals * (10^mids) / sum(vals * (10^mids))
+  baseline <- median(wdensity)
+
+  # Find highest density in upper half of barcode distribution
+  peak <- which(wdensity == max(wdensity[((length(wdensity)+1)/2):length(wdensity)]))
+
+  # Cutoff is the point before the peak at which density falls below 2X baseline
+  10^mids[max(which(wdensity[1:peak] < (2*baseline)))]
+}
