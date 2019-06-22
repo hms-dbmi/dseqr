9747222da9c9a289b27d1c897f68901041f6f6ea
alexvpickering <alexvpickering@gmail.com>
2019-06-21 17:17:06 -0700

    allow comparison to specific cluster; closes #80
    Former-commit-id: ffad9faed178d52cb6d02b4fd5afe61c48cc1342
    Former-commit-id: 50b467f6f11d28e82d3e6382dbec602f8566400c
    Former-commit-id: e2b70c1a08a0bdd67fb80318db85f0b006a30cbc

--- R/explore_scseq_clusters.R
+++ R/explore_scseq_clusters.R
@@ -11,19 +11,21 @@
 #'
 #' @examples
 #'
-#' # import alevin quants
+#' # import kallisto quants
 #' data_dir <- 'data-raw/single-cell/example-data/Run2644-10X-Lung/10X_FID12518_Normal_3hg'
-#' scseq <- load_scseq(data_dir, type = 'Seurat')
+#' scseq <- load_scseq(data_dir)
 #'
-#' # subset by alevin whitelist norm/stabilize using good cell only
+#' # subset by whitelist norm/stabilize using good cell only
 #' scseq <- scseq[, scseq$whitelist]
 #' scseq <- preprocess_scseq(scseq)
 #'
 #' # get clusters and run tSNE
-#' scseq <- add_scseq_clusters(scseq)
+#' scseq <- add_scseq_clusters(scseq, resolution = 1.6)
 #' scseq <- run_tsne(scseq)
 #'
-#' explore_scseq_clusters(scseq)
+#' markers <- get_scseq_markers(scseq)
+#'
+#' explore_scseq_clusters(scseq, markers)
 #'
 
 explore_scseq_clusters <- function(scseq, markers = NULL, assay.type = 'logcounts') {
@@ -36,17 +38,20 @@ explore_scseq_clusters <- function(scseq, markers = NULL, assay.type = 'logcount
   # use non-integrated but corrected assay for marker gene plots
   sce <- srt_to_sce(scseq, "SCT")
 
-  prev_gene <- NULL
+  # name cluster choices for drop down
   cluster_choices <- names(markers)
   names(cluster_choices) <- paste('Cluster', names(markers))
+
+  # placeholder for default cluster to compare to
+  default_contrast <- 'all'
+  names(default_contrast) <- 'All other clusters'
 
   #  user interface ----
 
   ui <- miniUI::miniPage(
     shiny::tags$head(
-      shiny::tags$style("#genecards, #subcluster {border-top-left-radius: 0; border-bottom-left-radius: 0; position: relative; z-index: 2; margin-left: -6px;}"),
-      shiny::tags$style("button.btn.btn-default.dropdown-toggle {background-color: transparent !important;border-top-right-radius: 0; border-bottom-right-radius: 0}"),
-      shiny::tags$style(".sub-chev {font-size: 10px; padding: 0 10px; color: darkgray;}")
+      shiny::tags$style("#genecards {border-top-left-radius: 0; border-bottom-left-radius: 0; position: relative; z-index: 2; margin-left: -6px;}"),
+      shiny::tags$style("button.btn.btn-default.dropdown-toggle {background-color: transparent !important;border-top-right-radius: 0; border-bottom-right-radius: 0}")
     ),
     miniUI::gadgetTitleBar("Explore Single-Cell Clusters"),
     miniUI::miniContentPanel(
@@ -54,6 +59,7 @@ explore_scseq_clusters <- function(scseq, markers = NULL, assay.type = 'logcount
         shiny::column(6,
                       shiny::uiOutput('groups_toggle'),
                       shiny::selectizeInput("selected_cluster", 'Show marker genes for:', choices = cluster_choices, width = '291.5px'),
+                      shiny::selectizeInput("contrast_cluster", 'In comparison to:', choices = NULL, width = '291.5px'),
                       shiny::br(),
                       shiny::tags$div(
                         shiny::tags$div(style = "display:inline-block;", id='gene-container', shinyWidgets::pickerInput('gene', 'Show expression for:', choices = NULL, width = '250px', options = shinyWidgets::pickerOptions(liveSearch = TRUE))),
@@ -80,6 +86,8 @@ explore_scseq_clusters <- function(scseq, markers = NULL, assay.type = 'logcount
 
     # reactive objects -----
 
+    reseting_con <- reactiveVal(FALSE)
+
     # used to determine available groups to show (e.g. ctrl and test)
     available_groups_r <- shiny::reactive({
       groups <- unique(as.character(sce$orig.ident))
@@ -96,12 +104,41 @@ explore_scseq_clusters <- function(scseq, markers = NULL, assay.type = 'logcount
       return(input$groups)
     })
 
+    # make it so that selected cluster can't contrast to itself
+    contrast_choices_r <- shiny::reactive({
+      contrast_choices <- c(default_contrast, cluster_choices)
+      contrast_choices <- contrast_choices[contrast_choices != input$selected_cluster]
+      return(contrast_choices)
+    })
 
-    # update marker genes based on cluster/subcluster selection -----
+    # change cluster to compare against
+    shiny::observeEvent(input$contrast_cluster, {
+
+      # default contrast is all (named as selected contrast)
+      id2 <- input$contrast_cluster
+      id1 <- contrast <- input$selected_cluster
+      if (id2 == '') return(NULL)
+
+      if (input$contrast_cluster != 'all') {
+
+        contrast <- paste0(id1, '-', id2)
+        if (!contrast %in% names(markers))
+          markers[[contrast]] <<- get_scseq_markers(scseq, ident.1 = id1, ident.2 = id2)
+      }
+
+      shinyWidgets::updatePickerInput(session, 'gene', choices = row.names(markers[[contrast]]))
+    })
+
+
+    # update marker genes based on cluster selection -----
     shiny::observe({
       cluster_markers <- markers[[input$selected_cluster]]
       choices <- row.names(cluster_markers)
       shinyWidgets::updatePickerInput(session, 'gene', choices = choices)
+
+      # reset contrast selections
+      shiny::updateSelectizeInput(session, 'contrast_cluster', choices = contrast_choices_r())
+
     })
 
 
