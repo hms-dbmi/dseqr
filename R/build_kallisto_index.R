61a449c15dd4bb6fbcb0d8e2e6f75e5fa42d10fb
alexvpickering <alexvpickering@gmail.com>
2019-06-25 16:23:20 -0700

    switch to kallisto for bulk rna seq; closes #79
    Former-commit-id: be1c6b765a3b3b18b7be508c97dc3d1a00cc7f0d
    Former-commit-id: 310e49cac724fe23f51e31afff4d66dbe7131017
    Former-commit-id: aa3d267fe07524a189a517a1465f6d1e7e852633

---
+++ R/build_kallisto_index.R
@@ -0,0 +1,52 @@
+#' Download ensembl transcriptome and build index for kalliso quantification
+#'
+#' This index is used for RNA seq quantification.
+#'
+#' @param species The species. Default is \code{homo_sapiens}.
+#' @param release ensembl release. Default is \code{94} (latest in release for AnnotationHub -
+#'   needs to match with \code{\link{build_ensdb}}).
+#' @param indices_dir Directory to put kallisto indices.
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' # build kallist index for humans
+#' indices_dir <- 'data-raw/indices/kallisto'
+#' build_ensdb_index(indices_dir)
+#'
+build_kallisto_index <- function(indices_dir, species = 'homo_sapiens', release = '94') {
+
+  if (!dir.exists(indices_dir))
+    dir.create(indices_dir, recursive = TRUE)
+
+  # construct ensembl url for transcriptome
+  ensembl_species <- gsub(' ', '_', tolower(species))
+  ensembl_release <- paste0('release-', release)
+  ensembl_url <- paste0('ftp://ftp.ensembl.org/pub/', ensembl_release, '/fasta/', ensembl_species, '/cdna/')
+
+  # get list of all files
+  handle <- curl::new_handle(dirlistonly=TRUE)
+  con <- curl::curl(ensembl_url, "r", handle)
+  tbl <- utils::read.table(con, stringsAsFactors=FALSE)
+  close(con)
+
+  # get transcripts cdna.all file
+  ensembl_fasta <- grep('cdna.all.fa.gz$', tbl[[1]], value = TRUE)
+  ensembl_url <- paste0(ensembl_url, ensembl_fasta)
+
+  work_dir <- getwd()
+  setwd(indices_dir)
+  curl::curl_download(ensembl_url, ensembl_fasta)
+
+  # build index
+  index_fname <- gsub('fa.gz$', paste0(ensembl_release, '_k31.idx'), ensembl_fasta)
+  index_fname <- tolower(index_fname)
+  tryCatch(system2('kallisto', args=c('index',
+                                      '-i', index_fname,
+                                      ensembl_fasta)),
+           error = function(err) {err$message <- 'Is kallisto installed and on the PATH?'; stop(err)})
+
+  unlink(ensembl_fasta)
+  setwd(work_dir)
+}
---
+++ R/run_kallisto_bulk.R
@@ -0,0 +1,99 @@
+#' Runs kallisto quantification for bulk samples.
+#'
+#' For pair-ended experiments, reads for each pair should be in a seperate file.
+#'
+#' @param indices_dir Directory with kallisto indices. See \code{\link{build_kallisto_index}}.
+#' @param data_dir Directory with raw fastq.gz RNA-Seq files.
+#' @param pdata_path Path to text file with sample annotations. Must be readable by \code{\link[data.table]{fread}}.
+#' The first column should contain sample ids that match a single raw rna-seq data file name.
+#' @param pdata Previous result of call to \code{run_salmon} or \code{\link{select_pairs}}. Used to bypass another call to \code{select_pairs}.
+#' @param species Species name. Default is \code{homo_sapiens}.
+#' Used to determine transcriptome index to use.
+#' @param fl.mean Estimated average fragment length (only relevant for single-end reads).
+#' @param fl.sd Estimated standard deviation of fragment length (only relevant for single-end reads).
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#'
+#' # first place IBD data in data-raw/example-data
+#' indices_dir <- 'data-raw/indices/kallisto'
+#' data_dir <- file.path('data-raw', 'example-data')
+#' pdata_path <- file.path(data_dir, 'Phenotypes.csv')
+#' run_kallisto_bulk(indices_dir, data_dir, pdata_path)
+#'
+run_kallisto_bulk <- function(indices_dir, data_dir, pdata_path = NULL, pdata = NULL, species = 'homo_sapiens', release = '94', fl.mean = NULL, fl.sd = NULL) {
+  # TODO: make it handle single and paired-end data
+  # now assumes single end
+
+  # get index_path
+  index_path <- file.path(indices_dir, paste0(species, '.grch38.cdna.all.release-', release, '_k31.idx'))
+
+  if (is.null(pdata_path) & is.null(pdata)) stop('One of pdata_path or pdata must be supplied.')
+
+  # prompt user to select pairs/validate file names etc
+  if (is.null(pdata)) pdata <- select_pairs(data_dir, pdata_path)
+  pdata$quants_dir <- gsub('.fastq.gz$', '', pdata$`File Name`)
+
+  # save selections
+  saveRDS(pdata, file.path(data_dir, 'pdata.rds'))
+
+  # save quants here
+  quants_dir <- file.path(data_dir, 'quants')
+  unlink(quants_dir, recursive = TRUE)
+  dir.create(quants_dir)
+
+  # specific flags for single end experiments
+  paired <- 'Pair' %in% colnames(pdata)
+  if (!paired) {
+    if (is.null(fl.mean)) {
+      message('Single-end experiment but estimated average fragment length not provided. Setting to 200.')
+      fl.mean <- 200
+    }
+
+    if (is.null(fl.sd)) {
+      message('Single-end experiment but estimated standard deviation of fragment length not provided. Setting to 20.')
+      fl.sd <- 20
+    }
+    flags <- c('--single', '-l', fl.mean, '-s', fl.sd)
+  }
+
+  # loop through fastq files and quantify
+  fastq_files <- pdata$`File Name`
+  if (is.null(fastq_files)) stop("File names should be in 'File Name' column")
+
+  while (length(fastq_files)) {
+
+    # grab next
+    fastq_file <-fastq_files[1]
+    row_num <- which(pdata$`File Name` == fastq_file)
+
+    # include any replicates
+    rep_num <- pdata$Replicate[row_num]
+    if (!is.null(rep_num) && !is.na(rep_num)) {
+      fastq_file <- pdata[pdata$Replicate %in% rep_num, 'File Name']
+    }
+
+    # remove fastq_files for next quant loop
+    fastq_files <- setdiff(fastq_files, fastq_file)
+
+    # possibly spaces in file names
+    fastq_path <- paste(shQuote(file.path(data_dir, fastq_file)), collapse = ' ')
+
+    # save each sample in it's own folder
+    # use the first file name in any replicates
+    sample_name <- gsub('.fastq.gz$', '', fastq_file[1])
+    out_dir <- file.path(quants_dir, sample_name)
+    dir.create(out_dir)
+
+    # run kallisto
+    system2('kallisto',
+            args=c('quant',
+                   '-i', index_path,
+                   '-o', shQuote(out_dir),
+                   flags,
+                   fastq_path))
+  }
+  return(NULL)
+}
