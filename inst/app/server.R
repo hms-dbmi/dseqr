91758a6bd78b705b2aa48ddd006aa114005d1163
alexvpickering <alexvpickering@gmail.com>
2019-07-11 17:43:12 -0700

    init moving select_pairs and select_contrast into app for #91
    Former-commit-id: a29e720eb583d82bd1c14b2c9355aedffc19f3f4
    Former-commit-id: 1ca063b8bb21be39d3a60997b619f047bb5cf2cd
    Former-commit-id: c94ea0c964af1502766ce616c205c3876531f23f

--- inst/app/server.R
+++ inst/app/server.R
@@ -1,12 +1,258 @@
+datasetsPage <- function(input, output, session) {
+  dsForm <- callModule(datasetsForm, 'form')
+
+  callModule(dsPairsTable, 'pairs', fastq_dir = dsForm$fastq_dir)
+}
+
+
+datasetsForm <- function(input, output, session) {
+
+  dsAnalysis <- callModule(dsAnalysis, 'selected_anal')
+
+  end_type <- callModule(dsEndType, 'end_type',
+                         fastq_dir = dsAnalysis$fastq_dir)
+
+  return(list(
+    fastq_dir = dsAnalysis$fastq_dir
+  ))
+
+}
+
+dsAnalysis <- function(input, output, session) {
+
+  indices_dir <- '~/Documents/Batcave/zaklab/drugseqr/data-raw/indices/kallisto'
+
+
+  # get directory with fastqs
+  bulk_dir <- c('bulk-data' = '~/Documents/Batcave/zaklab/drugseqr/data-raw/bulk/example-data')
+  shinyFiles::shinyDirChoose(input, "anal_dir", roots = bulk_dir)
+  fastq_dir <- reactiveVal('~/Documents/Batcave/zaklab/drugseqr/data-raw/bulk/example-data/IBD')
+
+  # fastq_dir <- reactive({
+  #   dir <- parseDirPath(bulk_dir, input$anal_dir)
+  #   req(dir)
+  #   dir
+  # })
+
+  anal_name <- reactive(input$anal_name)
+
+  return(list(
+    anal_name = anal_name,
+    fastq_dir = fastq_dir
+  ))
+
+}
+
+
+dsEndType <- function(input, output, session, fastq_dir) {
+
+  # get fastq files in directory
+  fastq_files <- reactive({
+    fastq_dir <- fastq_dir()
+    req(fastq_dir)
+
+    list.files(fastq_dir, '.fastq.gz$')
+  })
+
+  # auto detected if paired
+  detected_paired <- reactive({
+    fastqs <- fastq_files()
+    fastq_dir <- fastq_dir()
+    req(fastqs, fastq_dir)
+
+    # auto-detect if paired
+    fastq_id1s <- get_fastq_id1s(file.path(fastq_dir, fastqs))
+    detect_paired(fastq_id1s)
+  })
+
+
+
+  observe({
+    # label the end type choices with auto detected
+    end_types <- c('single-ended' = 'single-ended', 'pair-ended' = 'pair-ended')
+    if (detected_paired()) end_types <- end_types[c(2, 1)]
+    names(end_types)[1] <- paste(names(end_types)[1], '(detected)')
+
+    updateSelectizeInput(session, 'end_type', choices = end_types)
+  })
+
+  return(end_type = reactive(input$end_type))
+}
+
+
+dsPairsTable <- function(input, output, session, fastq_dir) {
+
+  # things user will update and return
+  state <- reactiveValues(pdata = 0)
+  pdata_r <- reactiveVal()
+  pairs_r <- reactiveVal()
+  reps_r <- reactiveVal()
+
+  # setup ----
+
+  # label button clicks
+  labels <- callModule(dsLabelRows, 'label_rows')
+
+  group_colors <- c("#C7E9C0", "#C6DBEF", "#FCBBA1", "#FDD0A2", "#BCBDDC", "#D9D9D9", "#F6E8C3", "#DC143C",
+                    "#A1D99B", "#9ECAE1", "#FC9272", "#FDAE6B", "#9E9AC8", "#BDBDBD", "#DFC27D", "#FFFFFF",
+                    "#C3B091", "#007FFF", "#00FFFF", "#7FFFD4", "#228B22", "#808000", "#7FFF00", "#BFFF00",
+                    "#FFD700", "#DAA520", "#FF7F50", "#FA8072","#FC0FC0", "#CC8899", "#E0B0FF", "#B57EDC", "#843179")
+
+  ncolors <- length(group_colors)
+  background <- 'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAPklEQVQoU43Myw0AIAgEUbdAq7VADCQaPyww55dBKyQiHZkzBIwQLqQzCk9E4Ytc6KEPMnTBCG2YIYMVpHAC84EnVbOkv3wAAAAASUVORK5CYII=) repeat'
+
+
+  # reset everything when new fastq_dir
+  observeEvent(fastq_dir(), {
+    fastq_dir <- fastq_dir()
+    req(fastq_dir)
+
+    fastqs <- list.files(fastq_dir, '.fastq.gz$')
+    pdata <- tibble::tibble('File Name' = fastqs)
+    pdata <- tibble::add_column(pdata, Group = NA, Pair = NA, Replicate = NA, .before = 1)
+
+    pdata_r(pdata)
+
+    clear <- rep(NA, nrow(pdata))
+    pairs_r(clear)
+    reps_r(clear)
+  })
+
+  # redraw table when new pdata (otherwise update data using proxy)
+  output$pdata <- DT::renderDataTable({
+
+    DT::datatable(
+      pdata_r(),
+      class = 'cell-border dt-fake-height',
+      rownames = FALSE,
+      escape = FALSE, # to allow HTML in table
+      options = list(
+        columnDefs = list(list(className = 'dt-nopad', targets = c(0, 1, 2))),
+        scrollY = FALSE,
+        paging = FALSE,
+        bInfo = 0
+      )
+    )
+  })
+
+
+  # proxy pdata to update Pair/Replicate column
+  proxy_pata <- shiny::eventReactive(state$pdata, {
+
+    pdata <- pdata_r()
+    reps <- reps_r()
+    pairs <- pairs_r()
+
+    # update pdata Replicate column
+    rep_nums <- sort(unique(setdiff(reps, NA)))
+    for (rep_num in rep_nums) {
+      color <- group_colors[ncolors - rep_num]
+      rows <- which(reps == rep_num)
+      pdata[rows, 'Replicate'] <- paste('<div style="background-color:', color, ';"></div>')
+    }
+
+    # update pdata Pair column
+    if (TRUE) {
+      pair_nums <- sort(unique(setdiff(pairs, NA)))
+      for (pair_num in pair_nums) {
+        color <- group_colors[pair_num]
+        rows <- which(pairs == pair_num)
+        pdata[rows, 'Pair'] <- paste('<div style="background:', color, background, ';"></div>')
+      }
+    } else {
+      pdata[1:nrow(pdata), 'Pair'] <- NA
+    }
+
+    return(pdata)
+  })
+
+  # proxy used to replace data
+  proxy <- DT::dataTableProxy("pdata")
+  shiny::observe({
+
+    DT::replaceData(proxy, proxy_pata(), rownames = FALSE)
+  })
+
+
+  # click 'Pair Samples' ----
+
+  shiny::observeEvent(labels$pair(), {
+    req(labels$pair())
+    reps <- reps_r()
+    pairs <- pairs_r()
+
+    # get rows
+    rows  <- input$pdata_rows_selected
+
+    # check for incomplete/wrong input
+    if (validate_pairs(pairs, rows, reps)) {
+
+      # add rows as a pair
+      pair_num <- length(unique(setdiff(pairs, NA))) + 1
+      pairs[rows] <- pair_num
+      pairs_r(pairs)
+
+      # update states to trigger updates
+      state$pdata <- state$pdata + 1
+    }
+  })
+
+  # click 'Mark Replicates' ----
+
+  shiny::observeEvent(labels$rep(), {
+    req(labels$rep())
+    reps <- reps_r()
+    pairs <- pairs_r()
+
+    # get rows
+    rows  <- input$pdata_rows_selected
+    if (validate_reps(pairs, rows, reps)) {
+      # add rows as replicates
+      rep_num <- length(unique(setdiff(reps, NA))) + 1
+      reps[rows] <- rep_num
+      reps_r(reps)
+
+      # update states to trigger updates
+      state$pdata <- state$pdata + 1
+    }
+  })
+
+
+  # click 'Reset' ----
+
+  shiny::observeEvent(input$reset, {
+    pdata <- pdata()
+    pairs <<- rep(NA, nrow(pdata))
+    reps <<- rep(NA, nrow(pdata))
+    pdata$Pair <<- NA
+    pdata$Replicate <<- NA
+
+    # remove groups from table and reset control
+    state$pdata <- state$pdata + 1
+  })
+}
+
+
+
+dsLabelRows <- function(input, output, session, fastq_dir) {
+
+  return(list(
+    test = reactive(input$test),
+    ctrl = reactive(input$ctrl),
+    reset = reactive(input$reset),
+    rep = reactive(input$rep),
+    pair = reactive(input$pair)
+  ))
+}
 
 #' Logic for Drugs page
-drugPage <- function(input, output, session) {
+drugsPage <- function(input, output, session) {
 
   # the form area inputs/results
-  form <- callModule(drugForm, 'form')
+  form <- callModule(drugsForm, 'form')
 
   # the output table
-  callModule(drugTable, 'table',
+  callModule(drugsTable, 'table',
              cmap_res = form$cmap_res,
              l1000_res = form$l1000_res,
              drug_study = form$drug_study,
@@ -19,7 +265,7 @@ drugPage <- function(input, output, session) {
 }
 
 # Logic for form on drugs page
-drugForm <- function(input, output, session) {
+drugsForm <- function(input, output, session) {
 
   drugStudy <- callModule(selectedDrugStudy, 'drug_study')
 
@@ -124,7 +370,7 @@ advancedOptions <- function(input, output, session, cmap_res, l1000_res, drug_st
 }
 
 #' Logic for drug table
-drugTable <- function(input, output, session, cmap_res, l1000_res, drug_study, cells, show_clinical) {
+drugsTable <- function(input, output, session, cmap_res, l1000_res, drug_study, cells, show_clinical) {
 
   # generate table to display
   query_res <- shiny::reactive({
@@ -204,7 +450,9 @@ server <- function(input, output, session) {
                        data_dir = data_dir)
 
 
-  drugPage <- callModule(drugPage, 'drug')
+  drugsPage <- callModule(drugsPage, 'drug')
+
+  datasetsPage <- callModule(datasetsPage, 'datasets')
 
 
 }
--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,23 +1,138 @@
-# UI for Drugs page
-drugPageUI <- function(id, tab, active) {
+
+datasetsPageUI <- function(id, tab, active) {
   ns <- NS(id)
-  active_class <- ifelse(tab == active, 'active', '')
+  withTags({
+    tabPane(tab, active,
+            div(class = 'row',
+                div(class = 'col-sm-6',
+                    datasetsFormInput(ns('form'))
+                )
+            ),
+            hr(),
+            div(dsPairsTable(ns('pairs')))
+    )
+  })
+}
+
+datasetsFormInput <- function(id) {
+  ns <- NS(id)
 
   withTags({
-    div(class = paste('tab-pane', active_class), `data-value` = tab, id = id_from_tab(tab),
-        div(class = 'row',
-            div(class = 'col-sm-6',
-                drugFormInput(ns('form'))
-            )
-        ),
-        hr(),
-        drugTableOutput(ns('table'))
+    div(class = "well-form well-bg",
+        dsAnalysisInput(ns('selected_anal')),
+        dsEndTypeInput(ns('end_type'))
+    )
+  })
+}
+
+
+dropdownMenuButton <- function(id, label) {
+  tags$li(
+    tags$a(
+      id = id, role = 'button', class = 'action-button shiny-bound-input', label
+    )
+  )
+}
+
+
+dsAnalysisInput <- function(id) {
+  ns <- NS(id)
+
+  withTags({
+    textInputWithButtons(ns('anal_name'), 'Analysis name:',
+                         button(id = ns('anal_dir'), type = 'button', class="shinyDirectories btn btn-default action-button shiny-bound-input",
+                                `data-title` = 'Folder with fastq.gz files',
+                                title = 'Select folder with fastq.gz files',
+                                i(class = 'far fa-folder')
+                         )
+    )
+  })
+}
+
+dsEndTypeInput <- function(id) {
+  ns <- NS(id)
+
+  selectizeInput(ns('end_type'),
+                 'Confirm end-type:',
+                 choices = NULL, width = '100%')
+}
+
+dsPairsTable <- function(id) {
+  ns <- NS(id)
+
+  tagList(
+    dsLabelRowsUI(ns('label_rows')),
+    DT::dataTableOutput(ns("pdata"))
+  )
+}
+
+dsLabelRowsUI <- function(id) {
+  ns <- NS(id)
+  withTags({
+    div(class = 'well-form no-well',
+         div(class = 'btn-group btn-group-justified',
+             div(class = 'btn-group',
+                 button(type = 'button',
+                        class = 'btn btn-default dropdown-toggle',
+                        `data-toggle`='dropdown',
+                        `aria-haspopup`='true',
+                        `aria-expanded`='false',
+                        span('Label Selected Rows')
+                 ),
+                 ul(class = 'dropdown-menu', style = 'width: 100%;',
+                    li(class="dropdown-header", 'Files'),
+                    dropdownMenuButton(ns('pair'), 'Paired'),
+                    dropdownMenuButton(ns('rep'), 'Replicates'),
+                    li(role = 'separator', class='divider'),
+                    li(class="dropdown-header", 'Group'),
+                    dropdownMenuButton(ns('test'), 'Test'),
+                    dropdownMenuButton(ns('ctrl'), 'Control'),
+                    li(role = 'separator', class='divider'),
+                    dropdownMenuButton(ns('reset'), 'Reset Labels')
+                 )
+             )
+         )
+
+    )
+  })
+}
+
+# utilities used throughout the site
+tabPane <- function(tab, active, ...) {
+  active_class <- ifelse(tab == active, 'active', '')
+  tags$div(class = paste('tab-pane', active_class), `data-value` = tab, id = id_from_tab(tab), ...)
+}
+
+textInputWithButtons <- function(id, label, ...) {
+  tags$div(class = 'form-group selectize-fh',
+           tags$label(class = 'control-label', `for` = id, label),
+           tags$div(class = 'input-group',
+                    tags$input(id = id, type = 'text', class = 'form-control shiny-bound-input', value = '', placeholder = ''),
+                    tags$span(class = 'input-group-btn', ...)
+           )
+  )
+}
+
+# -----
+# UI for Drugs page
+drugsPageUI <- function(id, tab, active) {
+  ns <- NS(id)
+
+  withTags({
+    tabPane(tab, active,
+            div(class = 'row',
+                div(class = 'col-sm-6',
+                    drugsFormInput(ns('form'))
+                )
+            ),
+            hr(),
+            drugsTableOutput(ns('table'))
     )
   })
 }
 
 # Output table for Drugs Page
-drugTableOutput <- function(id) {
+drugsTableOutput <- function(id) {
   ns <- NS(id)
 
   withTags({
@@ -28,7 +143,7 @@ drugTableOutput <- function(id) {
 }
 
 #' Input form for Drugs page
-drugFormInput <- function(id) {
+drugsFormInput <- function(id) {
   ns <- NS(id)
 
   withTags({
@@ -91,8 +206,9 @@ selectedDrugStudyInput <- function(id) {
 }
 
 
+# -----
 tabs <- c('Datasets', 'Single Cell', 'Bulk', 'Pathways', 'Drugs')
-active <- 'Drugs'
+active <- 'Datasets'
 
 bootstrapPage(
   useShinyjs(),
@@ -102,15 +218,16 @@ bootstrapPage(
   includeCSS(path = 'www/drugs.css'),
   navbarUI(tabs, active),
   fluidPage(
-    # make sure selectize loaded (not using default)
-    tags$div(style = 'display: none', selectizeInput('blah1', label = NULL, choices = '')),
+    # make sure css/js loaded from packages where not using functions (not using default)
+    tags$div(style = 'display: none;', selectizeInput('blah1', label = NULL, choices = '')),
+    tags$div(style = 'display: none;', shinyFiles::shinyDirButton("blah2", title='', label='', icon=icon('plus'))),
 
-    # THE TABS ----
     tags$div(class = "tab-content", `data-tabsetid` = "tabset", id = "tabs",
              # single cell tab
              scPageUI("sc", tab = 'Single Cell', active),
              bulkPageUI("bulk", tab = 'Bulk', active),
-             drugPageUI("drug", tab = 'Drugs', active)
+             drugsPageUI("drug", tab = 'Drugs', active),
+             datasetsPageUI('datasets', tab = 'Datasets', active)
     )
   )
 )
