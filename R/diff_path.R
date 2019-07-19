e1d4591e8a22224767a8f2ee4c99166367c66549
alexvpickering <alexvpickering@gmail.com>
2019-07-19 12:04:45 -0700

    init running PADOG for RNA-Seq; closes #98
    Former-commit-id: 000bc537fa560c1b2db25bd8bd2f4bf3192cc376
    Former-commit-id: 07e07d7738f3dba6cca760b7bb258cb90eab2477
    Former-commit-id: a0164fc459845c0ce75cdd2d5957b5df40ba80d7

---
+++ R/diff_path.R
@@ -0,0 +1,53 @@
+#' Differential expression of KEGG pathways.
+#'
+#' Performs PADOG pathway analysis using KEGG database.
+#' PADOG outperforms other pathway analysis algorithms at prioritizing expected pathways (see references).
+#'
+#' @param eset Expression set.
+#' @param prev_anal Previous result of \code{\link{diff_expr}}.
+#'
+#' @references Tarca AL, Bhatti G, Romero R. A Comparison of Gene Set Analysis Methods
+#'    in Terms of Sensitivity, Prioritization and Specificity. Chen L, ed. PLoS ONE.
+#'    2013;8(11):e79217. doi:10.1371/journal.pone.0079217.
+#'
+#'    Dong X, Hao Y, Wang X, Tian W. LEGO: a novel method for gene set over-representation
+#'    analysis by incorporating network-based gene weights. Scientific Reports.
+#'    2016;6:18871. doi:10.1038/srep18871.
+#'
+#' @export
+#'
+#' @examples
+#' eset <- readRDS('/home/alex/Documents/Batcave/zaklab/drugseqr/data-raw/patient_data/sjia/bulk/mono/eset.rds')
+#' prev_anal <- readRDS('/home/alex/Documents/Batcave/zaklab/drugseqr/data-raw/patient_data/sjia/bulk/mono/diff_expr_symbol_ferritin.rds')
+#'
+#' diff_path(eset, prev_anal)
+#'
+diff_path <- function(eset, prev_anal, rna_seq = TRUE){
+
+  # remove replicates/duplicates and annotate with human ENTREZID
+  dups <- iqr_replicates(eset, annot = 'ENTREZID_HS', rm.dup = TRUE)
+  eset <- dups$eset
+
+  # subset eset to previously analysis samples
+  prev_pdata <- prev_anal$pdata
+  eset <- eset[, row.names(prev_pdata)]
+
+  groups <- prev_pdata$group
+
+  # contrast levels
+  ctrl <- 'ctrl'
+  test <- 'test'
+
+  # group
+  incon <- groups %in% c(ctrl, test)
+  group <- ifelse(groups[incon] == ctrl, 'c', 'd')
+
+  # other padog inputs
+  esetm  <- exprs(eset[, incon])
+
+  # run padog
+  padog_table <- PADOG::padog(esetm = esetm, group = group, parallel = TRUE, ncr = 4,
+                              verbose = FALSE, rna_seq = rna_seq, pdata = prev_pdata)
+
+  return(padog_table)
+}
