7befc802a4fb45f227204d49c59a9353f64814f0
Alex Pickering <alexvpickering@gmail.com>
2019-10-16 13:39:18 -0400

    add conda to PATH for Rstudio session
    Former-commit-id: 9f497318cf1bc1904ff76bd536e0d3cf02233f46
    Former-commit-id: 6a93f468e83cf54fad12ee5f2041282babe23c89
    Former-commit-id: c197d679f83a648aa541abd586c9a0bce527e0a9

--- scripts/setup-aws.sh
+++ scripts/setup-aws.sh
@@ -4,7 +4,12 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 echo 'export PATH=$HOME/miniconda/bin:$PATH' >> ~/.bashrc
+
+# add miniconda to PATH
 source ~/.bashrc
+
+# add miniconda to PATH for Rstudio session (local)
+echo 'export PATH=${HOME}/miniconda/bin:${PATH}' >> ~/.Renviron
 
 # setup bioconda channels
 conda config --add channels defaults
