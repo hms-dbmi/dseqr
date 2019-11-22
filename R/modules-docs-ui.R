65a06e5b46228ea2112c2f3c461ea2bf22f83f14
Alex Pickering <alexvpickering@gmail.com>
2019-11-22 17:29:28 -0500

    add some filler for docs page #147
    Former-commit-id: 3a507d914c18c442657dde6c0c38ca7c20254c14 [formerly 9c4156ad42a49d9510dd3ca51063a03e3c3c1429]
    Former-commit-id: 6466a166fc6f1175ebc86629c803005a365169d9
    Former-commit-id: 4e94265a07d6642cc75ce303f96cb2e194970b4d

--- R/modules-docs-ui.R
+++ R/modules-docs-ui.R
@@ -1,33 +1,106 @@
 #' UI for docs page (add new sections here)
 #' @export
 #' @keywords internal
+
+#' UI for docs page (add new sections here)
+#' @export
+#' @keywords internal
 docsPageUI <- function(id, tab, active) {
-  ns <- NS(id)
-
   docsSections <- list(
-    # Overview section
-    docsSection(id = 'js-overview', name = 'Overview',
+    # Datasets documentation
+    docsSection(id = 'ds-docs', name = 'Datasets',
                 content = list(
+                  docsSubsection(id = 'ds-quantification',
+                                 name = 'Quantifying RNA-seq datasets',
+                                 content = tagList(
+                                   p("To add a new dataset, first make sure the dataset name input is empty. Then type
+                                   a name for your new bulk or single-cell dataset and hit enter. You will be prompted to select a folder
+                                   containing either fastq.gz files (bulk or single-cell) or CellRanger files (single-cell)."),
+                                   p("If a folder with bulk fastq.gz files is selected, you will be prompted to confirm the auto-selected end type.
+                                     If the end type is pair-ended, you will also need to indicate pairs. To do this, click the rows corresponding to the
+                                     paired files and click Paired. If present, you should also indicate any files that are replicates of the same sample before you run quantification."),
+                                   p("If a folder with single-cell fastq.gz files is selected, nothing further is necessary before running quantification. Quantification uses kallisto for both bulk and single-cell RNA-Seq data.
+                                     After quantification completes, bulk datasets can be selected in the Datasets tab for differential expression and exploratory analyses whereas single-cell datasets
+                                     will show up in the Single Cell tab for exploratory analyses and sample integration.")
+                                 )
 
-                  docsSubsection(id = 'js-individual-compiled',
-                                 name = 'Individual or compiled',
-                                 content = tagList(p(shinipsum::random_text(nwords=500)),
-                                                   p(shinipsum::random_text(nwords=500)))
                   ),
-                  docsSubsection(id = 'js-data-attrs',
-                                 name = 'Data attributes',
-                                 content = tagList(p(shinipsum::random_text(nwords=500)),
-                                                   p(shinipsum::random_text(nwords=500)))
+                  docsSubsection(id = 'ds-differential-expression',
+                                 name = 'Differential expression',
+                                 content = tagList(
+                                   p("For quantified bulk RNA-Seq datasets, select the differential expression analysis type in order to run
+                                     differential expression analyses between two groups. Any differential expression analyses performed will be accessible
+                                     in the Pathways and Drugs tab for further analysis."),
+                                   p("")
+                                 )
                   ),
-                  docsSubsection(id = 'js-programmatic-api',
-                                 name = 'Programmatic API',
-                                 content = tagList(p(shinipsum::random_text(nwords=500)),
-                                                   p(shinipsum::random_text(nwords=500)))
+                  docsSubsection(id = 'ds-gene-plots',
+                                 name = 'Sample-level gene expression',
+                                 content = tagList(p(shinipsum::random_text(nwords=100)),
+                                                   p(shinipsum::random_text(nwords=100)))
                   ),
-                  docsSubsection(id = 'js-noconflict',
-                                 name = 'No conflict',
-                                 content = tagList(p(shinipsum::random_text(nwords=500)),
-                                                   p(shinipsum::random_text(nwords=500)))
+                  docsSubsection(id = 'ds-cell-plots',
+                                 name = 'Cell-type deconvolution',
+                                 content = tagList(p(shinipsum::random_text(nwords=100)),
+                                                   p(shinipsum::random_text(nwords=100)))
+                  )
+                )),
+
+    # Single cell documentation
+    docsSection(id = 'sc-docs', name = 'Single Cell',
+                content = list(
+                  docsSubsection(id = 'sc-label-clusters',
+                                 name = 'Labeling cell clusters',
+                                 content = tagList(p(shinipsum::random_text(nwords=100)),
+                                                   p(shinipsum::random_text(nwords=100)))
+
+                  ),
+                  docsSubsection(id = 'sc-integrate-samples',
+                                 name = 'Integrate datasets',
+                                 content = tagList(p(shinipsum::random_text(nwords=100)),
+                                                   p(shinipsum::random_text(nwords=100)))
+                  ),
+                  docsSubsection(id = 'sc-differential-expression',
+                                 name = 'Differential expression analysis',
+                                 content = tagList(p(shinipsum::random_text(nwords=100)),
+                                                   p(shinipsum::random_text(nwords=100)))
+                  )
+                )),
+
+    # Pathways documentation
+    docsSection(id = 'path-docs', name = 'Pathways',
+                content = list(
+                  docsSubsection(id = 'path-overview',
+                                 name = 'Overview',
+                                 content = tagList(p(shinipsum::random_text(nwords=100)),
+                                                   p(shinipsum::random_text(nwords=100)))
+
+                  ),
+                  docsSubsection(id = 'path-query',
+                                 name = 'CMAP02/L1000 query genes',
+                                 content = tagList(p(shinipsum::random_text(nwords=100)),
+                                                   p(shinipsum::random_text(nwords=100)))
+                  )
+                )),
+
+    # Drugs documentation
+    docsSection(id = 'drugs-docs', name = 'Drugs',
+                content = list(
+                  docsSubsection(id = 'drugs-overview',
+                                 name = 'Overview',
+                                 content = tagList(p(shinipsum::random_text(nwords=100)),
+                                                   p(shinipsum::random_text(nwords=100)))
+
+                  ),
+                  docsSubsection(id = 'drugs-custom-queries',
+                                 name = 'Custom queries',
+                                 content = tagList(p(shinipsum::random_text(nwords=100)),
+                                                   p(shinipsum::random_text(nwords=100)))
+                  ),
+                  docsSubsection(id = 'drugs-advanced',
+                                 name = 'Advanced options',
+                                 content = tagList(p(shinipsum::random_text(nwords=100)),
+                                                   p(shinipsum::random_text(nwords=100)))
                   )
                 ))
   )
@@ -38,10 +111,10 @@ docsPageUI <- function(id, tab, active) {
 
   withTags({
     tabPane(tab, active,
-            div(class = "container bs-docs-container",
+            div(class = "bs-docs-container",
                 div(class = 'row',
-                    div(class = 'col-md-8', role="main", docsSections),
-                    div(class = "col-md-3 col-md-offset-1 bd-toc", role="complementary", docsSideNav(section_info))
+                    div(class = 'col-md-6 col-md-offset-1', role="main", docsSections),
+                    div(class = "col-md-4 col-md-offset-1 bd-toc", role="complementary", docsSideNav(section_info))
                 )
             )
     )
