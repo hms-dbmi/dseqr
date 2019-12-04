1cd157d8f2f3c45da5cd78e68bd8095ea1de661e
Alex Pickering <alexvpickering@gmail.com>
2019-12-04 11:57:00 -0500

    add functionality to factor out surrogate variables
    Former-commit-id: 30af1694f5c034e8484bb0d43c564cd99b814789 [formerly 284f4fce15b3b4d082c833e877dd0cc1758ccebb]
    Former-commit-id: 4ba9de87c9960e4cc339cc24a679e96849bd3f3b
    Former-commit-id: 19ab0ef7aeea1be1e56551c344fbc6af44d37f92

---
+++ R/remove_sva.R
@@ -0,0 +1,43 @@
+
+#' Run surrogate variable analysis and factor out surrogate variables
+#'
+#' Returned result usefull for heatmaps, MDS plots, machine learning, etc
+#'
+#' @param eset ExpressionSet with group column in \code{pData(eset)}
+#'
+#' @return matrix of vsd (if RNA-seq) RMA (microarray) normalized expression with
+#'  surrogate variables factored out
+#' @export
+remove_sva <- function(eset) {
+
+  # determine if this is rna seq data
+  rna_seq <- 'norm.factors' %in% colnames(Biobase::pData(eset))
+
+  # make full and null model matrix
+  group_levels = unique(Biobase::pData(eset)$group)
+  group <- factor(Biobase::pData(eset)$group, levels = group_levels)
+
+  mod <- stats::model.matrix(~0 + group)
+  mod0 <- stats::model.matrix(~1, data = group)
+
+  colnames(mod)[1:length(group_levels)] <- group_levels
+
+
+  # remove duplicated rows (from 1:many PROBE:SYMBOL) as affect sva
+  PROBE <- Biobase::fData(eset)[,1]
+  expr <- unique(data.table::data.table(Biobase::exprs(eset), PROBE))[, PROBE := NULL]
+  expr <- as.matrix(expr)
+
+  # sva or svaseq
+  sva_fun <-sva::svaseq
+  svobj <- sva::svaseq(expr, mod, mod0)
+
+  # get DESeq::vsd transformed counts for RNA-Seq
+  el <- ifelse(rna_seq, 'vsd', 'exprs')
+  vsd <- Biobase::assayDataElement(eset, el)
+
+  # return cleaned
+  vsd_clean <- clean_y(vsd, mod, svobj$sv)
+  return(vsd_clean)
+
+}
