d3c07252bef1b6270cef9859dc4fef6e510b1c8b
Alex Pickering <alexvpickering@gmail.com>
2019-12-10 10:52:27 -0500

    allow adjusting for pairs+surrogate variables or just pairs
    Former-commit-id: 3399efd504253ce675060317aafffa66e0d7505c [formerly ff77f231fcc0314de0a5b69239bf829619997a1e]
    Former-commit-id: 8ba28bcc50d51c1f867f1582a461db9c8f9eaaae
    Former-commit-id: 2c8b892883292eaf5c2877129f8f48a04af2c597

--- R/remove_sva.R
+++ R/remove_sva.R
@@ -1,14 +1,15 @@
 
-#' Run surrogate variable analysis and factor out surrogate variables
+#' Run surrogate variable analysis and factor out surrogate variables and any pairs
 #'
-#' Returned result usefull for heatmaps, MDS plots, machine learning, etc
+#' Returned result useful for heatmaps, MDS plots, machine learning, etc
 #'
-#' @param eset ExpressionSet with group column in \code{pData(eset)}
+#' @param eset ExpressionSet with group column in \code{pData(eset)} and optional pair column.
+#' @param run_sva If \code{FALSE} (default \code{TRUE}) then only regresses out pair column.
 #'
 #' @return matrix of vsd (if RNA-seq) RMA (microarray) normalized expression with
-#'  surrogate variables factored out
+#'  surrogate variables and optional pairs factored out
 #' @export
-remove_sva <- function(eset) {
+remove_sva <- function(eset, run_sva = TRUE) {
 
   # determine if this is rna seq data
   rna_seq <- 'norm.factors' %in% colnames(Biobase::pData(eset))
@@ -16,14 +17,25 @@ remove_sva <- function(eset) {
   # make full and null model matrix
   group_levels = unique(Biobase::pData(eset)$group)
   group <- factor(Biobase::pData(eset)$group, levels = group_levels)
-  pair <- Biobase::pData(eset)$pair
+  pair <- factor(as.numeric(Biobase::pData(eset)$pair))
 
+  if (length(pair)) {
+    mod <- stats::model.matrix(~0 + group + pair)
+    mod0 <- stats::model.matrix(~1 + pair, data = group)
+    colnames(mod)[1:length(group_levels)] <- group_levels
 
-  mod <- stats::model.matrix(~0 + group)
-  mod0 <- stats::model.matrix(~1, data = group)
+    # remove non matched pairs
+    has.pair <- colSums(mod0[, 2:ncol(mod0)]) >= 2
+    has.pair <- names(has.pair[has.pair])
 
-  colnames(mod)[1:length(group_levels)] <- group_levels
+    mod <- mod[, c(group_levels, has.pair)]
+    mod0 <- mod0[, c("(Intercept)", has.pair)]
 
+  } else {
+    mod <- stats::model.matrix(~0 + group)
+    mod0 <- stats::model.matrix(~1, data = group)
+    colnames(mod)[1:length(group_levels)] <- group_levels
+  }
 
   # remove duplicated rows (from 1:many PROBE:SYMBOL) as affect sva
   PROBE <- Biobase::fData(eset)[,1]
@@ -32,14 +44,23 @@ remove_sva <- function(eset) {
 
   # sva or svaseq
   sva_fun <-ifelse(rna_seq, sva::svaseq, sva::sva)
-  svobj <- sva_fun(expr, mod, mod0)
+  if (run_sva) {
+    svobj <- sva_fun(expr, mod, mod0)
+  } else {
+    svobj <- list(sv = NULL)
+  }
 
   # get DESeq::vsd transformed counts for RNA-Seq
   el <- ifelse(rna_seq, 'vsd', 'exprs')
   vsd <- Biobase::assayDataElement(eset, el)
 
-  # return cleaned
-  vsd_clean <- clean_y(vsd, mod, svobj$sv)
-  return(vsd_clean)
+  # clean svs and pairs
+  if (length(pair)) {
+    to_clean <- cbind(svobj$sv, mod0[, has.pair])
+  } else {
+    to_clean <- svobj$sv
+  }
 
+  vsd_clean <- clean_y(vsd, mod[, group_levels], to_clean)
+  return(vsd_clean)
 }
