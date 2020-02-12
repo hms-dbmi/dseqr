efdc51b3b3d0a8ee60d4bb5b245d672ad294d5ce
Alex Pickering <alexvpickering@gmail.com>
2020-02-12 14:42:30 -0500

    whitelisting uses emptyDrops first
    Found a workaround for emptyDrops calling massive numbers of cells:
    https://github.com/MarioniLab/DropletUtils/issues/33
    
    TLDR: remove mito/ribo genes before emptyDrops
    
    Former-commit-id: 2b300f3e77c1ec56461181d872ca66e2192f64eb [formerly 0098425163113b3dfbc936a668f9b685d82d8a57]
    Former-commit-id: a77fd264b8ca92d91c367fbbf7b9aa4570e25bc0
    Former-commit-id: ba8baab047d8c13dfdb3d0f1aa90528913d23ef7

--- R/get_scseq_whitelist.R
+++ R/get_scseq_whitelist.R
@@ -15,175 +15,36 @@ get_scseq_whitelist <- function(counts, data_dir, overwrite = TRUE, species = 'H
     return(whitelist)
   }
 
-  # based on salmon alevin paper
-  # get knee and keep at least 1000 below it
   ncount <- Matrix::colSums(counts)
-  ncount.ord <- order(ncount, decreasing = TRUE)
-  counts <- counts[, ncount.ord]
-  ncount <- ncount[ncount.ord]
 
-  knee <- find_knee(counts)
+  # if already filtered cellranger matrix, can't call if empty
+  if (min(ncount) < 10) {
 
-  nabove <- sum(ncount > knee)
-  nbelow <- max(0.2*nabove, 1000)
+    # omit mito/ribo genes for cell calling (see MarioniLab/DropletUtils#33)
+    qc <- load_scseq_qcgenes(species)
+    keep <- !row.names(counts) %in% unlist(qc)
 
-  # if already filtered cellranger matrix file then keep everything
-  if (min(ncount) > 10) {
-    keep <- seq_len(ncol(counts))
-
-  } else {
-    keep <- 1:(nabove + nbelow)
+    set.seed(100)
+    e.out <- DropletUtils::emptyDrops(counts[keep, ], retain = Inf)
+    counts <- counts[, which(e.out$FDR <= 0.001)]
   }
 
   # add qc metrics
-  sce <- SingleCellExperiment::SingleCellExperiment(assays = list(counts = counts[, keep]))
+  sce <- SingleCellExperiment::SingleCellExperiment(assays = list(counts = counts))
   sce <- add_scseq_qc_metrics(sce, species = species)
 
   # setup stats for outlier detection
-  df <- as.data.frame(sce@colData)
-  stats <- df[, c('sum', 'detected', 'subsets_mito_percent', 'subsets_ribo_percent')]
-  stats$sum <- log10(stats$sum)
-  stats$detected <- log10(stats$detected)
+  df <- sce@colData
+  stats <- cbind(log10(df$sum), log10(df$detected), df$subsets_mito_percent, df$subsets_ribo_percent)
 
-  if (min(ncount) > 10) {
-    # if filtered cellranger then just drop outliers, no modeling
-    outlying <- robustbase::adjOutlyingness(stats, only.outlyingness = TRUE)
-    outl.drop <- scater::isOutlier(outlying, type = "higher")
+  outlying <- robustbase::adjOutlyingness(stats, only.outlyingness = TRUE)
+  outl.drop <- scater::isOutlier(outlying, type = "higher")
 
-    # set cell with high outlyingness as low quality
-    df$quality <- 'high'
-    df[row.names(stats)[outl.drop], 'quality'] <- 'low'
-
-  } else {
-    # detect outliers above knee only
-    stats <- stats[1:nabove, ]
-    outlying <- robustbase::adjOutlyingness(stats, only.outlyingness = TRUE)
-    outl.drop <- scater::isOutlier(outlying, type = "higher")
-
-    df$quality <- NA
-    df$quality[sce$total <= knee] <- 'low'
-
-    # devide above evenly into high and ambiguous
-    midpnt <- round(nabove/2)
-    df$quality[1:midpnt] <- 'high'
-    df$quality[(midpnt+1):nabove] <- 'ambig'
-
-    # set outliers above the knee as low quality
-    df[row.names(stats)[outl.drop], 'quality'] <- 'low'
-
-    # clean df
-    nunique <- function(x) { length(unique(na.omit(x))) != 1 }
-    df <- df[, apply(df, 2, nunique)]
-    df <- df[, !duplicated(t(df))]
-
-    # run model/get preds
-    df$quality <- factor(df$quality)
-    train <- df[df$quality != 'ambig', ]
-    test  <- df[df$quality == 'ambig', ]
-
-    svm_model <- e1071::svm(quality ~ ., data = train)
-    svm_preds <- predict(svm_model, newdata = test)
-
-    # determine what to keep and save for future
-    test$quality <- svm_preds
-    df <- rbind(train, test)
-  }
-
-  whitelist <- row.names(df)[df$quality == 'high']
-  kneelist  <- row.names(stats)
+  kneelist  <- colnames(sce)
+  whitelist <- colnames(sce)[!outl.drop]
+  message('keeping ', length(whitelist), '/', length(kneelist), ' non-empty droplets.')
 
   writeLines(whitelist, whitelist_path)
   writeLines(kneelist, file.path(data_dir, 'kneelist.txt'))
   return(whitelist)
-}
-
-#' Find knee point by looking for convergance of roryk and DropletUtils
-#'
-#' @param counts dgCMatrix
-#'
-#' @return Knee point
-#' @export
-#' @keywords internal
-find_knee <- function(counts) {
-  ncount <- Matrix::colSums(counts)
-  ncount.ord <- order(ncount, decreasing = TRUE)
-  counts <- counts[, ncount.ord]
-  ncount <- ncount[ncount.ord]
-
-  iter <- 1
-  done <- FALSE
-  keep <- seq_len(ncol(counts))
-
-  # stop if distance between knee and inflection is more than twice between the knees
-  while(!done & iter < 100) {
-    knee <- get_knee(counts[, keep])
-    done <- abs(knee$knee - knee$inflection) > abs(knee$roryk- knee$knee)*2
-    keep <- head(keep, round(length(keep)*0.9))
-    iter <- iter + 1
-  }
-
-  # if didn't converge pick smallest above inflection
-  if (iter == 100) knee <- get_knee(counts)
-  if (knee$roryk < knee$inflection) knee$roryk <- knee$knee
-
-  min(knee$roryk, knee$knee)
-}
-
-
-#' Get knee point and plot on barcode rank plot
-#'
-#' @inheritParams get_scseq_whitelist
-#'
-#' @return count value corresponding to knee point
-#' @export
-#' @keywords internal
-get_knee <- function(counts) {
-  ncount <- Matrix::colSums(counts)
-  bcrank <- DropletUtils::barcodeRanks(counts)
-
-  # Only showing unique points for plotting speed.
-  uniq <- !duplicated(bcrank$rank)
-
-  plot(bcrank$rank[uniq], bcrank$total[uniq], log="xy",
-       xlab="Rank", ylab="Total UMI count", cex.lab=1.2)
-
-  roryk <- pick_roryk_cutoff(bcrank$total)
-
-  abline(h=bcrank@metadata$inflection, col="darkgreen", lty=2)
-  abline(h=bcrank@metadata$knee, col="dodgerblue", lty=2)
-  abline(h=roryk, col="red", lty=2)
-
-  legend("bottomleft", legend=c("Inflection", "Knee", "Roryk Knee"),
-         col=c("darkgreen", "dodgerblue", "red"), lty=2, cex=1.2)
-
-  inflection <- bcrank@metadata$inflection
-  knee <- bcrank@metadata$knee
-
-  return(list(
-    knee = knee,
-    roryk = roryk,
-    inflection = inflection))
-}
-
-#' Pick Roryk knee point
-#'
-#' from https://github.com/COMBINE-lab/salmon/issues/362
-#'
-#' @param bcs Vector of total counts for all barcodes
-#'
-#' @return count value corresponding to Roryk knee point
-#' @export
-#' @keywords internal
-pick_roryk_cutoff <- function(bcs){
-  bcs_hist = hist(log10(bcs), plot=FALSE, n=50)
-  mids = bcs_hist$mids
-  vals = bcs_hist$count
-  wdensity = vals * (10^mids) / sum(vals * (10^mids))
-  baseline <- median(wdensity)
-
-  # Find highest density in upper half of barcode distribution
-  peak <- which(wdensity == max(wdensity[((length(wdensity)+1)/2):length(wdensity)]))
-
-  # Cutoff is the point before the peak at which density falls below 2X baseline
-  10^mids[max(which(wdensity[1:peak] < (2*baseline)))]
 }
