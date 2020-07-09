329e91ec3869b221139b482910c7729cc0e739ca
Alex Pickering <alexvpickering@gmail.com>
2020-07-09 10:07:49 -0700

    update heatplot colors
    Former-commit-id: 16cc4a238665149a26758cec06f1b35e12d9ffa4 [formerly 6ca927bce50ee649aac27cbeaf7c09dc74824518]
    Former-commit-id: c3c732656c9385f91cdff555cea88ad7be1d0cc6
    Former-commit-id: 0ba5944eb156d1fc4e9b2932fc877c6a4b5bf923

--- R/heatplot.R
+++ R/heatplot.R
@@ -18,7 +18,12 @@ heatplot <- function(path_res, foldChange = NULL) {
     d$foldChange <- foldChange[as.character(d[,2])]
     p <- ggplot2::ggplot(d, ggplot2::aes_(~Gene, ~categoryID)) +
       ggplot2::geom_tile(ggplot2::aes_(fill = ~foldChange), color = "white") +
-      ggplot2::scale_fill_continuous(low="blue", high="red", name = "fold change")
+      # ggplot2::scale_fill_continuous(low="blue", high="red", name = "fold change")
+      ggplot2::scale_fill_gradient2(
+        low = "#00FF00",
+        mid = "white",
+        high = "#FF0000",
+        midpoint = 0)
 
   } else {
     p <- ggplot2::ggplot(d, ggplot2::aes_(~Gene, ~categoryID)) +
@@ -26,9 +31,8 @@ heatplot <- function(path_res, foldChange = NULL) {
   }
   p + ggplot2::xlab(NULL) +
     ggplot2::ylab(NULL) +
-    ggplot2::theme_minimal() +
-    ggplot2::theme(panel.grid.major = ggplot2::element_blank(),
-                   axis.text.x=ggplot2::element_text(angle = 60, hjust = 1))
+    ggplot2::theme_linedraw() +
+    ggplot2::theme(axis.text.x=ggplot2::element_text(angle = 60, hjust = 1))
 }
 
 extract_geneSets <- function(path_res) {
