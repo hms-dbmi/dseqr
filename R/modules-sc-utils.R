cf791b8cee60adfc008036da6896a95e5e2ccf12
Alex Pickering <alexvpickering@gmail.com>
2021-01-14 17:50:11 -0800

    show progress for integ/subset/import

--- R/modules-sc-utils.R
+++ R/modules-sc-utils.R
@@ -778,7 +778,7 @@ integrate_saved_scseqs <- function(
   # add ambient outlier info
   combined <- add_integrated_ambient(combined, ambient)
 
-  progress$set(value+5, detail = 'getting markers')
+  progress$set(value+5, detail = 'cluster markers')
   tests <- pairwise_wilcox(combined, block = combined$batch, groups = combined$cluster)
   markers <- get_scseq_markers(tests)
 
@@ -792,7 +792,7 @@ integrate_saved_scseqs <- function(
                                            batch = combined$batch))
 
 
-  progress$set(value+6, detail = 'fitting linear models')
+  progress$set(value+6, detail = 'comparing groups')
   obj <- combined
   pbulk_esets <- NULL
   has_replicates <- length(unique(combined$batch)) > 2
@@ -887,11 +887,6 @@ subset_saved_scseq <- function(sc_dir, founder, from_dataset, dataset_name, excl
     process_raw_scseq(scseq, dataset_name, sc_dir, progress = progress, value = 1, founder = founder)
     save_scseq_args(args, dataset_name, sc_dir)
   }
-
-  return(list(
-    from_dataset = from_dataset,
-    dataset_name = dataset_name,
-    is_integrated = is_integrated))
 }
 
 
