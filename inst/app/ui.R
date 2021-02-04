e60776cb578b1befea13fd4fc96f2881de921e48
Alex Pickering <alexvpickering@gmail.com>
2021-02-04 09:52:36 -0800

    fix favicon url

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -20,7 +20,7 @@ bootstrapPage(
   navbarUI(tabs, active, with_logout),
   fluidPage(
     tags$head(HTML("<title>drugseqr</title>"),
-              tags$link(rel = "icon", type = "image/png", href = "/favicon.png")),
+              tags$link(rel = "icon", type = "image/png", href = "https://raw.githubusercontent.com/hms-dbmi/drugseqr.sp/master/favicon.png")),
     tags$div(class = "tab-content shiny-bound-input", `data-tabsetid` = "tabset", id = "tabs",
              # rintrojs stuff
              span(id = 'start_tour', class='action-button shiny-bound-input btn-intro-icon btn-intro', icon('info', 'fa-fw fa-w-6')),
