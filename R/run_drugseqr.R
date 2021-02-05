af9faeabb513abf67787f7b015c683a93f3d5c3d
Alex Pickering <alexvpickering@gmail.com>
2021-02-04 17:50:42 -0800

    blah

--- R/run_drugseqr.R
+++ R/run_drugseqr.R
@@ -51,7 +51,7 @@ run_drugseqr <- function(app_name,
   if (!dir.exists(data_dir)) dir.create(data_dir)
 
   # pass arguments to app through options then run
-  shinyOptions(data_dir = normalizePath(data_dir),
+  shiny::shinyOptions(data_dir = normalizePath(data_dir),
                       pert_query_dir = normalizePath(pert_query_dir),
                       pert_signature_dir = normalizePath(pert_signature_dir),
                       indices_dir = normalizePath(indices_dir),
@@ -72,8 +72,9 @@ run_drugseqr <- function(app_name,
   }
 
   # auto-reload if update app files
-  options(shiny.autoreload = TRUE)
-  runApp(app_dir, launch.browser = TRUE, host = host, port = port)
+  if (grepl('/srv/drugseqr', data_dir)) options(shiny.autoreload = TRUE)
+
+  shiny::runApp(app_dir, launch.browser = TRUE, host = host, port = port)
 }
 
 
