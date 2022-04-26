41e8ba071db8e5ad2f889cb501f9863556376461
Alex Pickering <alexvpickering@gmail.com>
2022-04-25 21:17:03 -0700

    roxygenize

--- data-raw/convert/0.30.0.R
+++ data-raw/convert/0.30.0.R
@@ -1,10 +1,10 @@
 # add default project dirs
 # remove sample metrics from cdata
 
-data_dir <- '/home/alex/Documents/Data/dseqr'
+data_dir <- '/srv/dseqr'
 app_dirs <- list.files(data_dir)
 app_dirs <- setdiff(app_dirs,
-                    c('indices', 'node_modules', 'pert_signature_dir',
+                    c('indices', 'node_modules', 'pert_signature_dir', 'tmp',
                       'pert_query_dir', 'gs_dir', 'example_data.tar.gz', 'tx2gene'))
 
 
@@ -17,6 +17,6 @@ for  (app_dir in app_dirs) {
   if (dir.exists(default_dir)) next()
   dir.create(default_dir)
 
-  file.move(from = file.path(data_dir, app_dir, move_dirs),
-            to = file.path(default_dir, move_dirs))
+  file.rename(from = file.path(data_dir, app_dir, move_dirs),
+              to = file.path(default_dir, move_dirs))
 }
