60a2c7cd9bfa19737240524edc89e5bc73c8cafa
Alex Pickering <alexvpickering@gmail.com>
2022-05-02 13:28:34 -0700

    fix app test

--- inst/app/tests/testthat/test-shinytest2.R
+++ inst/app/tests/testthat/test-shinytest2.R
@@ -1,20 +1,19 @@
 library(shinytest2)
 library(SingleCellExperiment)
-library(org.Hs.eg.db)
 
 # create single-cell dataset for upload
 mock_10x_files <- function() {
 
   set.seed(0)
-  sce <- scDblFinder::mockDoubletSCE(ncells = c(200, 300, 100, 200, 300), ngenes = 10000)
+  ngenes <- 10000
+  sce <- scDblFinder::mockDoubletSCE(ncells = c(200, 300, 100, 200, 300), ngenes = ngenes)
   counts <- as(counts(sce), 'dgCMatrix')
-  ensids <- keys(org.Hs.eg.db, keytype = 'ENSEMBL')[1:10000]
-  genes <- mapIds(org.Hs.eg.db, ensids, column = 'SYMBOL', keytype = 'ENSEMBL')
+  t2g <- dseqr.data::load_tx2gene()
   DropletUtils::write10xCounts(
     path = 'mock_10x',
     x = counts,
-    gene.id = ensids,
-    gene.symbol = genes
+    gene.id = t2g$gene_id[seq_len(ngenes)],
+    gene.symbol = t2g$gene_name[seq_len(ngenes)]
   )
 
   for (file in list.files('mock_10x', full.names = TRUE)) R.utils::gzip(file)
@@ -22,18 +21,20 @@ mock_10x_files <- function() {
 
 
 test_that("{shinytest2} recording: import_sc", {
+  suppressWarnings(
   app <- AppDriver$new(
     name = "import_sc",
     options = list(shiny.maxRequestSize = 30*1024*1024^2),
     width = 1619,
     height = 909,
     seed = 0)
+  )
 
   app$wait_for_idle()
 
   mock_10x_files()
   upload_dir <- 'mock_10x'
-  on.exit(unlink(uploads_dir, recursive = TRUE), add = TRUE)
+  on.exit(unlink(upload_dir, recursive = TRUE), add = TRUE)
   on.exit(unlink('test_data_dir', recursive = TRUE), add = TRUE)
 
   app$upload_file(`sc-form-dataset-up_raw` = file.path(upload_dir, "barcodes.tsv.gz"))
