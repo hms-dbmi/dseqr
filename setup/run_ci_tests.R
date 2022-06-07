dd34fea7b4d3deb3fdc61535441a9a005d906054
Alex Pickering <alexvpickering@gmail.com>
2022-06-06 20:23:32 -0700

    try override test skip

--- setup/run_ci_tests.R
+++ setup/run_ci_tests.R
@@ -1,3 +1,4 @@
+Sys.setenv('NOT_CRAN' = TRUE)
 args <- c('--no-manual', '--no-build-vignettes')
 rcmdcheck::rcmdcheck(error_on = 'error', build_args = args, args = args)
 testthat::test_file('inst/app/tests/testthat/test-shinytest2.R')
